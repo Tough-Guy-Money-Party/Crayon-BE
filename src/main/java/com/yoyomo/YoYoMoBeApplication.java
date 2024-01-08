d3fcbba12bc50a51093c4a2c7873e91ac8bdf68f
ay-eonii <naayen@naver.com>
2024-01-08 22:20:17 +0900

    feat: Initial project

---
+++ src/main/java/com/yoyomo/YoYoMoBeApplication.java
@@ -0,0 +1,13 @@
+package com.yoyomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoYoMoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoYoMoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoyomo/YoYoMoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoyomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoYoMoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
