5009b4fbfbb22aa8f633eb5cb16c440c66dec3de
ay-eonii <naayen@naver.com>
2024-01-12 13:56:52 +0900

    feat: club 응답 메시지

---
+++ src/main/java/com/yoyomo/domain/club/presentation/constant/ResponseMessage.java
@@ -0,0 +1,16 @@
+package com.yoyomo.domain.club.presentation.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseMessage {
+    SUCCESS_CREATE("동아리 생성에 성공했습니다."),
+    SUCCESS_READ("동아리 조회에 성공했습니다."),
+    SUCCESS_UPDATE("동아리 수정에 성공했습니다."),
+    SUCCESS_DELETE("동아리 삭제에 성공했습니다."),
+    FAIL("fail"),
+    ;
+    private String message;
+}
