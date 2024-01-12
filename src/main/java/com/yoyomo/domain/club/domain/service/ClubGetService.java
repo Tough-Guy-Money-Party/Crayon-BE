abecfa8e0414176e85f7bda1aaa6cc1f55a7b74e
ay-eonii <naayen@naver.com>
2024-01-12 13:52:22 +0900

    feat: club 조회 서비스

---
+++ src/main/java/com/yoyomo/domain/club/domain/service/ClubGetService.java
@@ -0,0 +1,7 @@
+package com.yoyomo.domain.club.domain.service;
+
+import com.yoyomo.domain.club.domain.entity.Club;
+
+public interface ClubGetService {
+    Club byId(String id);
+}
---
+++ src/main/java/com/yoyomo/domain/club/domain/service/ClubGetServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yoyomo.domain.club.domain.service;
+
+import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.club.domain.repository.ClubRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClubGetServiceImpl implements ClubGetService {
+    private final ClubRepository clubRepository;
+
+    public Club byId(String id) {
+        return clubRepository.findById(id).orElseThrow();
+    }
+}
