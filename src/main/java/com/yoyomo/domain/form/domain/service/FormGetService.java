ed82e1477df93bc691fd48091c7c213733bcd598
ay-eonii <naayen@naver.com>
2024-01-14 00:53:19 +0900

    feat: 지원폼 조회 서비스

---
+++ src/main/java/com/yoyomo/domain/form/domain/service/FormGetService.java
@@ -0,0 +1,16 @@
+package com.yoyomo.domain.form.domain.service;
+
+import com.yoyomo.domain.form.domain.entity.Form;
+import com.yoyomo.domain.form.domain.repository.FormRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FormGetService {
+    private final FormRepository formRepository;
+
+    public Form byId(String id) {
+        return formRepository.findById(id).orElseThrow();
+    }
+}
