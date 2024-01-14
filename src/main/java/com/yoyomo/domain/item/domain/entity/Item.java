ef8a05e0b0650a4b98a3b6ff01059aa4b36b7e35
ay-eonii <naayen@naver.com>
2024-01-14 15:29:10 +0900

    feat: 항목 엔티티 추가 radio, checkbox는 select short, long은 text

--- src/main/java/com/yoyomo/domain/item/domain/entity/Item.java
+++ src/main/java/com/yoyomo/domain/item/domain/entity/Item.java
@@ -1,9 +1,12 @@
 package com.yoyomo.domain.item.domain.entity;
 
+import com.yoyomo.domain.item.domain.entity.type.Type;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
 @Getter
@@ -12,7 +15,10 @@ import org.springframework.data.annotation.Id;
 @AllArgsConstructor
 public class Item {
     @Id
-    private String id;
+    @Builder.Default
+    private ObjectId id = ObjectId.get();
     private String question;
+    private Type type;
+    private int order;
     private boolean required;
 }
---
+++ src/main/java/com/yoyomo/domain/item/domain/entity/type/Type.java
@@ -0,0 +1,5 @@
+package com.yoyomo.domain.item.domain.entity.type;
+
+public enum Type {
+    SHORT, LONG, RADIO, CHECKBOX
+}
