8ad8dc8d6c7ddebb3ac56e1c44c4b8a45e66268f
ay-eonii <naayen@naver.com>
2024-01-14 16:05:06 +0900

    fix: club 유스케이스를 인터페이스화

---
+++ src/main/java/com/yoyomo/domain/club/application/usecase/ClubManageUseCaseImpl.java
@@ -0,0 +1,42 @@
+package com.yoyomo.domain.club.application.usecase;
+
+import com.yoyomo.domain.club.application.dto.req.ClubRequest;
+import com.yoyomo.domain.club.application.dto.res.ClubCreateResponse;
+import com.yoyomo.domain.club.application.dto.res.ClubResponse;
+import com.yoyomo.domain.club.application.mapper.ClubMapper;
+import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.club.domain.service.ClubGetService;
+import com.yoyomo.domain.club.domain.service.ClubSaveService;
+import com.yoyomo.domain.club.domain.service.ClubUpdateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ClubManageUseCaseImpl implements ClubManageUseCase {
+    private final ClubGetService clubGetService;
+    private final ClubSaveService clubSaveService;
+    private final ClubUpdateService updateService;
+    private final ClubMapper clubMapper;
+
+    public ClubResponse read(String id) {
+        Club club = clubGetService.byId(id);
+        return clubMapper.clubToClubResponse(club);
+    }
+
+    public ClubCreateResponse create(ClubRequest request) {
+        Club club = clubMapper.from(request);
+        String id = clubSaveService.save(club).getId();
+        return new ClubCreateResponse(id);
+    }
+
+    public void update(String id, ClubRequest request) {
+        updateService.from(id, request);
+    }
+
+    public void delete(String id) {
+        updateService.delete(id);
+    }
+}
--- src/main/java/com/yoyomo/domain/club/domain/service/ClubSaveService.java
+++ src/main/java/com/yoyomo/domain/club/domain/service/ClubSaveService.java
@@ -1,7 +1,16 @@
 package com.yoyomo.domain.club.domain.service;
 
 import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.club.domain.repository.ClubRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface ClubSaveService {
-    Club save(Club club);
+@Service
+@RequiredArgsConstructor
+public class ClubSaveService {
+    private final ClubRepository clubRepository;
+
+    public Club save(Club club) {
+        return clubRepository.save(club);
+    }
 }
