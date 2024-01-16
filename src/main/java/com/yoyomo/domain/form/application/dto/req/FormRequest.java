2e53376387edb668cc43d9452f39fe643331d6df
ay-eonii <naayen@naver.com>
2024-01-16 16:43:01 +0900

    feat: 지원폼 `process` 필드 추가

--- src/main/java/com/yoyomo/domain/form/application/dto/req/FormRequest.java
+++ src/main/java/com/yoyomo/domain/form/application/dto/req/FormRequest.java
@@ -1,7 +1,10 @@
 package com.yoyomo.domain.form.application.dto.req;
 
+import java.util.List;
+
 public record FormRequest(
         String clubId,
-        String name
+        String name,
+        List<String> process
 ) {
 }
