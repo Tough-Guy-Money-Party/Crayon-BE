a942a488f8413842cb093dd2e0c3fce9fd591139
ay-eonii <naayen@naver.com>
2024-01-16 16:44:08 +0900

    fix: update 동적 매핑 로직 오류 수정 id 매핑 제외

--- src/main/java/com/yoyomo/domain/shared/util/MapperUtil.java
+++ src/main/java/com/yoyomo/domain/shared/util/MapperUtil.java
@@ -7,17 +7,22 @@ import org.springframework.data.mongodb.core.query.Update;
 import java.beans.PropertyDescriptor;
 
 public class MapperUtil {
-
     private static final String CLASS = "class";
+    private static final String ID = "id";
+    private static final String PROPERTY_FORMAT = "%s%s";
 
     public static Update mapToUpdate(Object request) {
+        return mapToUpdate(request, "");
+    }
+
+    public static Update mapToUpdate(Object request, String pathPrefix) {
         Update update = new Update();
         BeanWrapper beanWrapper = new BeanWrapperImpl(request);
         for (PropertyDescriptor pd : beanWrapper.getPropertyDescriptors()) {
-            String propertyName = pd.getName();
-            Object propertyValue = beanWrapper.getPropertyValue(propertyName);
+            String propertyName = String.format(PROPERTY_FORMAT, pathPrefix, pd.getName());
+            Object propertyValue = beanWrapper.getPropertyValue(pd.getName());
 
-            if (propertyValue != null && !propertyName.equals(CLASS)) {
+            if (propertyValue != null && !propertyName.contains(CLASS) && !propertyName.contains(ID)) {
                 update.set(propertyName, propertyValue);
             }
         }
