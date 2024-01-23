8eeec2b70a78b8363d000c87eb90be634804258b
ay-eonii <naayen@naver.com>
2024-01-23 14:27:48 +0900

    fix: 지원 request 필드 추가 모집ID, 응답, 제출상태

--- src/main/java/com/yoyomo/domain/application/application/dto/req/ApplicationRequest.java
+++ src/main/java/com/yoyomo/domain/application/application/dto/req/ApplicationRequest.java
@@ -1,6 +1,12 @@
 package com.yoyomo.domain.application.application.dto.req;
 
+import com.yoyomo.domain.application.domain.entity.SubmitStatus;
+
 import java.util.Map;
 
-public record ApplicationRequest(Map<String, String> answers) {
+public record ApplicationRequest(
+        String recruitmentId,
+        Map<String, String> answers,
+        SubmitStatus submitStatus
+) {
 }
