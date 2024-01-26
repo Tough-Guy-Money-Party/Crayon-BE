5bfed35d2f10ccff7e09d8e20a341921da854531
taeseok yang <136783693+taeseokyang@users.noreply.github.com>
2024-01-26 19:39:00 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,5 +18,5 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
-. ~/.bashrc
+. /etc/profile
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
