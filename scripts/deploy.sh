c9066f2476cfc21d9377a269e624e816f7c970d4
taeseok yang <136783693+taeseokyang@users.noreply.github.com>
2024-01-26 19:34:43 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 REPOSITORY=/home/app/deploy
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
@@ -17,4 +17,6 @@ echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
+
+. ~/.bashrc
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
