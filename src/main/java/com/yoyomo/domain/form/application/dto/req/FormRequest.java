cb8f045c4958c675b34071d7974050ea9ada7d08
taeseokyang <ts.yang.0123@gmail.com>
2024-02-18 15:53:30 +0900

    feat: 폼 엔티티 description컬럼 추가

--- src/main/java/com/yoyomo/domain/form/application/dto/req/FormRequest.java
+++ src/main/java/com/yoyomo/domain/form/application/dto/req/FormRequest.java
@@ -7,6 +7,7 @@ import java.util.List;
 public record FormRequest(
         String clubId,
         String name,
+        String description,
         List<ItemRequest> itemRequests
 ) {
 }
--- src/main/java/com/yoyomo/domain/form/domain/entity/Form.java
+++ src/main/java/com/yoyomo/domain/form/domain/entity/Form.java
@@ -25,6 +25,8 @@ public class Form {
 
     private String name;
 
+    private String description;
+
     private List<Item> items;
 
     private LocalDateTime deletedAt;
