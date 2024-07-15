1dbbe95fc711baf3d87ef5f23cb674217ffe15ea
rootTiket <l20078829@gmail.com>
2024-07-15 19:14:43 +0900

    refactor: 함수 분할

--- src/main/java/com/yoyomo/domain/image/application/ImageService.java
+++ src/main/java/com/yoyomo/domain/image/application/ImageService.java
@@ -1,37 +1,64 @@
 package com.yoyomo.domain.image.application;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.yoyomo.domain.image.exception.ImageSaveFailureException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
 
-    @Value("${image.path}")
-    private String IMAGE_PATH;
-    public String DEFAULT_IMAGE = "default.png";
+    @Value("${application.bucket.name}")
+    private String BUCKETNAME;
 
-    public String save(MultipartFile pic) {
-        if (pic == null) {
+    public String DEFAULT_IMAGE = "default.png";
+    private final AmazonS3Client s3Client;
+
+    public String save(MultipartFile image) {
+        if (image == null) {
             return DEFAULT_IMAGE;
         }
-        UUID uuid = UUID.randomUUID();
-        String imageFileName = uuid + pic.getOriginalFilename();
-        Path imagePath = Paths.get(IMAGE_PATH + imageFileName);
-        try {
-            Files.write(imagePath, pic.getBytes());
-        } catch (Exception e) {
+
+        String fileName = getFileName(image);
+        String key = "image/" + fileName;
+
+        try (InputStream inputStream = image.getInputStream()) {
+            s3Client.putObject(new PutObjectRequest(BUCKETNAME, key, inputStream, getMetadataByImage(image))
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
             throw new ImageSaveFailureException();
         }
-        return imageFileName;
+
+        String storeFileUrl = s3Client.getUrl(BUCKETNAME, key).getPath();
+
+        return storeFileUrl;
     }
+
+    private String getFileName(MultipartFile image) {
+        String originalFilename = image.getOriginalFilename();
+        int index = originalFilename.lastIndexOf(".");
+        String ext = originalFilename.substring(index + 1);
+
+        String storeFileName = UUID.randomUUID() + "." + ext;
+        return storeFileName;
+    }
+
+    private ObjectMetadata getMetadataByImage(MultipartFile image){
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(image.getContentType());
+        objectMetadata.setContentLength(image.getSize());
+        return objectMetadata;
+    }
+
 }
 
--- src/main/java/com/yoyomo/domain/image/presentation/ImageController.java
+++ src/main/java/com/yoyomo/domain/image/presentation/ImageController.java
@@ -1,26 +1,23 @@
 package com.yoyomo.domain.image.presentation;
 
 import com.yoyomo.domain.image.application.ImageService;
+import com.yoyomo.global.config.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
+    private final ImageService imageService;
 
-    @Value("${image.path}")
-    private String IMAGE_PATH;
-
-    @GetMapping("/image/{path}")
-    public ResponseEntity<?> returnImage(@PathVariable String path) {
-        Resource resource = new FileSystemResource(IMAGE_PATH + path);
-        return new ResponseEntity<>(resource, HttpStatus.OK);
+    @PostMapping("/image")
+    public ResponseDto<String> returnImage(@RequestParam MultipartFile pic) {
+        return ResponseDto.of(HttpStatus.OK.value(),"성공",imageService.save(pic));
     }
 }
