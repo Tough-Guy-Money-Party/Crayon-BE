b840041cfcd0078c7fd2852a42f3dd27af761455
koungq <guu119@naver.com>
2024-07-17 00:09:12 +0900

    Chore: 불필요한 import 삭제

--- src/main/java/com/yoyomo/domain/recruitment/application/dto/res/RecruitmentDetailsResponse.java
+++ src/main/java/com/yoyomo/domain/recruitment/application/dto/res/RecruitmentDetailsResponse.java
@@ -1,6 +1,5 @@
 package com.yoyomo.domain.recruitment.application.dto.res;
 
-import com.yoyomo.domain.form.domain.entity.Form;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 
 import java.util.List;
