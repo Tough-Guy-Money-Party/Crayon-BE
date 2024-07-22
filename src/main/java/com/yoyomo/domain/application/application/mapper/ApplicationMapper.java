483bbe4bb2b740f28b5e80b85b66963dfc195fba
koungq <guu119@naver.com>
2024-07-22 17:28:18 +0900

    refactor: 기존 지원자 조회 API에서 평가 상태 및 현재 stage 타이틀까지 조회하여 필요한 API 합병

--- src/main/java/com/yoyomo/domain/application/application/mapper/ApplicationMapper.java
+++ src/main/java/com/yoyomo/domain/application/application/mapper/ApplicationMapper.java
@@ -6,9 +6,10 @@ import com.yoyomo.domain.application.application.dto.res.ApplicationManageRespon
 import com.yoyomo.domain.application.application.dto.res.ApplicationResponse;
 import com.yoyomo.domain.application.application.dto.res.MyApplicationsResponse;
 import com.yoyomo.domain.application.domain.entity.Application;
-import com.yoyomo.domain.application.domain.entity.AssessmentStatus;
 import com.yoyomo.domain.application.domain.service.ApplicationGetService;
+import com.yoyomo.domain.application.exception.ApplicationNotFoundException;
 import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
 import com.yoyomo.domain.user.domain.entity.Applicant;
 import org.mapstruct.*;
@@ -30,8 +31,18 @@ public interface ApplicationMapper {
     @Mapping(target = "id", source = "application.id")
     MyApplicationsResponse mapToMyApplications(Application application, Club club);
 
+    @Mapping(target = "currentStageTitle", source = "application", qualifiedByName = "findStageTitle")
     ApplicationResponse mapToApplicationResponse(Application application);
 
     @Mapping(target = "currentStageApplicants", expression = "java( applicationGetService.findApplicantsByStage(application.getRecruitment().getId(), application.getCurrentStage()))")
     ApplicationManageResponse mapToApplicationManage(Application application, @Context ApplicationGetService applicationGetService);
+
+    @Named("findStageTitle")
+    default String findStageTitle(Application application) {
+        return application.getRecruitment().getProcesses().stream()
+                .filter(process -> process.getStage() == application.getCurrentStage())
+                .findAny()
+                .map(Process::getTitle)
+                .orElseThrow(ApplicationNotFoundException::new);
+    }
 }
