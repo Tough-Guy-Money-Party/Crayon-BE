14c30681192a7048f5fa68e05c054e75af2a98f1
rootTiket <l20078829@gmail.com>
2024-07-22 15:25:21 +0900

    feat: 포괄 설정 조회 로직 구현

--- src/main/java/com/yoyomo/domain/club/application/usecase/ClubManageUseCase.java
+++ src/main/java/com/yoyomo/domain/club/application/usecase/ClubManageUseCase.java
@@ -3,10 +3,7 @@ package com.yoyomo.domain.club.application.usecase;
 import com.yoyomo.domain.club.application.dto.req.ClubRequest;
 import com.yoyomo.domain.club.application.dto.req.ParticipationRequest;
 import com.yoyomo.domain.club.application.dto.req.RemoveManagerRequest;
-import com.yoyomo.domain.club.application.dto.res.ClubCreateResponse;
-import com.yoyomo.domain.club.application.dto.res.ClubManagerResponse;
-import com.yoyomo.domain.club.application.dto.res.ClubResponse;
-import com.yoyomo.domain.club.application.dto.res.ParticipationResponse;
+import com.yoyomo.domain.club.application.dto.res.*;
 import org.springframework.security.core.Authentication;
 
 import java.util.List;
@@ -19,6 +16,8 @@ public interface ClubManageUseCase {
     ParticipationResponse participate(ParticipationRequest participateRequest, String userEmail);
 
     List<ClubManagerResponse> getManagers(Authentication authentication);
+
+    ClubGeneralSettingResponse getGeneralSetting(Authentication authentication);
 
     void removeManager(RemoveManagerRequest removeManagerRequest);
 
