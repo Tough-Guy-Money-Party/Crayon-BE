c1fcc276f9236873b4b6cdb27f493ff306c03609
rootTiket <l20078829@gmail.com>
2024-07-23 20:17:18 +0900

    feat: 응답 dto 생성 usecase 구현

--- src/main/java/com/yoyomo/domain/club/application/usecase/LandingManageUseCaseImpl.java
+++ src/main/java/com/yoyomo/domain/club/application/usecase/LandingManageUseCaseImpl.java
@@ -3,20 +3,13 @@ package com.yoyomo.domain.club.application.usecase;
 import com.yoyomo.domain.club.application.dto.req.UpdateGeneralSettingsRequest;
 import com.yoyomo.domain.club.application.dto.req.UpdateStyleSettingsRequest;
 import com.yoyomo.domain.club.application.dto.res.ClubGeneralSettingResponse;
+import com.yoyomo.domain.club.application.dto.res.ClubStyleSettingsResponse;
 import com.yoyomo.domain.club.application.mapper.ClubMapper;
 import com.yoyomo.domain.club.application.mapper.ClubStyleMapper;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.club.domain.service.ClubGetService;
-import com.yoyomo.domain.club.domain.service.ClubSaveService;
 import com.yoyomo.domain.club.domain.service.ClubUpdateService;
-import com.yoyomo.domain.club.exception.ClubNotFoundException;
-import com.yoyomo.domain.user.domain.entity.Manager;
-import com.yoyomo.domain.user.domain.repository.ManagerRepository;
-import com.yoyomo.domain.user.domain.service.UserGetService;
-import com.yoyomo.domain.user.exception.UserNotFoundException;
-import com.yoyomo.global.config.participation.service.ParticipationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,13 +20,16 @@ public class LandingManageUseCaseImpl implements LandingManageUseCase{
     private final ClubUpdateService clubUpdateService;
     private final ClubGetService clubGetService;
     private final ClubMapper clubMapper;
-    private final ManagerRepository managerRepository;
-    private final UserGetService userGetService;
     private final ClubStyleMapper clubStyleMapper;
 
     @Override
     public ClubGeneralSettingResponse getGeneralSetting(String email) {
         return clubMapper.clubToClubGeneralSettingResponse(clubGetService.byUserEmail(email));
+    }
+
+    @Override
+    public ClubStyleSettingsResponse getStyleSetting(String email) {
+        return clubStyleMapper.ClubLandingStyleToClubStyleSettingsResponse(clubGetService.byUserEmail(email).getClubLandingStyle());
     }
 
     public void update(String email, UpdateGeneralSettingsRequest request) {
