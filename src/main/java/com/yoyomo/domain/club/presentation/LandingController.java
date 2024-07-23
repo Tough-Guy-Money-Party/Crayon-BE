d2f58885efc20aaf4afef1cc3f3410cb456bc3db
rootTiket <l20078829@gmail.com>
2024-07-23 18:46:44 +0900

    feat: 스타일 변경 컨트롤러 구현

--- src/main/java/com/yoyomo/domain/club/presentation/LandingController.java
+++ src/main/java/com/yoyomo/domain/club/presentation/LandingController.java
@@ -2,6 +2,7 @@ package com.yoyomo.domain.club.presentation;
 
 
 import com.yoyomo.domain.club.application.dto.req.UpdateGeneralSettingsRequest;
+import com.yoyomo.domain.club.application.dto.req.UpdateStyleSettingsRequest;
 import com.yoyomo.domain.club.application.usecase.ClubManageUseCaseImpl;
 import com.yoyomo.global.config.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,20 +25,27 @@ public class LandingController {
     @PostMapping
     @Operation(summary = "노션 링크 등록")
     public ResponseDto create(Authentication authentication, @RequestParam String notionPageLink) {
-        clubManageUseCase.create(authentication,notionPageLink);
+        clubManageUseCase.create(authentication, notionPageLink);
         return ResponseDto.of(OK.value(), SUCCESS_UPDATE.getMessage());
     }
 
     @PatchMapping("/general")
     @Operation(summary = "동아리 포괄 설정")
     public ResponseDto update(Authentication authentication, @RequestBody UpdateGeneralSettingsRequest updateGeneralSettingsRequest) {
-        clubManageUseCase.update(authentication,updateGeneralSettingsRequest);
+        clubManageUseCase.update(authentication, updateGeneralSettingsRequest);
         return ResponseDto.of(OK.value(), SUCCESS_UPDATE.getMessage());
     }
 
     @GetMapping("/general")
     @Operation(summary = "동아리 포괄 설정 가져오기")
     public ResponseDto getGeneralSettings(Authentication authentication) {
-        return ResponseDto.of(OK.value(),SUCCESS_READ.getMessage(),clubManageUseCase.getGeneralSetting(authentication));
+        return ResponseDto.of(OK.value(), SUCCESS_READ.getMessage(), clubManageUseCase.getGeneralSetting(authentication));
+    }
+
+    @PatchMapping("/style")
+    @Operation(summary = "동아리 스타일 설정")
+    public ResponseDto update(Authentication authentication, @RequestBody UpdateStyleSettingsRequest updateStyleSettingsRequest) {
+        clubManageUseCase.update(updateStyleSettingsRequest, authentication.getName());
+        return ResponseDto.of(OK.value(), SUCCESS_UPDATE.getMessage());
     }
 }
