02aeee15a74f447cd3ad27a576f23c653c4dbf6f
koungq <guu119@naver.com>
2024-07-28 22:25:02 +0900

    Refactor: 지원 DTO 수정

--- src/main/java/com/yoyomo/domain/application/application/dto/req/AnswerRequest.java
+++
@@ -1,7 +0,0 @@
-package com.yoyomo.domain.application.application.dto.req;
-
-public record AnswerRequest(
-        String itemId,
-        String reply
-) {
-}
--- src/main/java/com/yoyomo/domain/application/application/mapper/ApplicationMapper.java
+++ src/main/java/com/yoyomo/domain/application/application/mapper/ApplicationMapper.java
@@ -1,21 +1,16 @@
 package com.yoyomo.domain.application.application.mapper;
 
 import com.yoyomo.domain.application.application.dto.req.ApplicationRequest;
-import com.yoyomo.domain.application.application.dto.res.ApplicationDetailsResponse;
-import com.yoyomo.domain.application.application.dto.res.ApplicationManageResponse;
-import com.yoyomo.domain.application.application.dto.res.ApplicationResponse;
-import com.yoyomo.domain.application.application.dto.res.MyApplicationsResponse;
+import com.yoyomo.domain.application.application.dto.res.*;
 import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.exception.ApplicationNotFoundException;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.interview.application.dto.InterviewResponse;
-import com.yoyomo.domain.item.application.dto.res.ItemResponse;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
 import com.yoyomo.domain.user.domain.entity.Applicant;
 import org.mapstruct.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @Mapper(componentModel = "spring",
@@ -38,9 +33,10 @@ public interface ApplicationMapper {
     @Mapping(target = "currentStageTitle", expression = "java( findStageTitle(application, recruitment) )")
     ApplicationResponse mapToApplicationResponse(Application application, Recruitment recruitment);
 
-    @Mapping(target = "items", expression = "java( getItemResponses(recruitment) )")
     @Mapping(target = "interview", expression = "java( getInterviewResponseDto(application) )")
     ApplicationManageResponse mapToApplicationManage(Application application, Recruitment recruitment);
+
+
 
     @Named("findStageTitle")
     default String findStageTitle(Application application, Recruitment recruitment) {
@@ -56,12 +52,5 @@ public interface ApplicationMapper {
         return Optional.ofNullable(application.getInterview())
                 .map(interview -> new InterviewResponse(interview.getPlace(), interview.getDate()))
                 .orElse(null);  // 인터뷰 절차가 아니라면 인터뷰 데이터는 비워둬야 하므로
-    }
-
-    @Named("getItemResponses")
-    default List<ItemResponse> getItemResponses(Recruitment recruitment) {
-        return recruitment.getForm().getItems().stream()
-                .map(ItemResponse::new)
-                .toList();
     }
 }
