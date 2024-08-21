857de0558b4e2f896c61b5c452b66be3d5c59010
koungq <guu119@naver.com>
2024-08-21 16:29:11 +0900

    chore: 불필요한 클래스 제거

--- src/main/java/com/yoyomo/domain/form/application/mapper/FormMapper.java
+++ src/main/java/com/yoyomo/domain/form/application/mapper/FormMapper.java
@@ -1,7 +1,6 @@
 package com.yoyomo.domain.form.application.mapper;
 
 import com.yoyomo.domain.form.application.dto.request.FormRequestDTO;
-import com.yoyomo.domain.form.application.dto.request.FormUpdateRequest;
 import com.yoyomo.domain.form.domain.entity.Form;
 import com.yoyomo.domain.item.domain.entity.Item;
 import org.mapstruct.Mapper;
@@ -19,8 +18,6 @@ import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.Re
 public interface FormMapper {
 
     Form from(FormRequestDTO.Save dto, List<Item> items, String clubId);
-
-    Form from(FormUpdateRequest request, List<Item> items);
 
     DetailResponse toDetailResponse(Form form);
 
