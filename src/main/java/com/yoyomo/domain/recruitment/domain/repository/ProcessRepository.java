b4e50ab872485f4be1fda6cdf437e87236f74d7f
koungq <guu119@naver.com>
2024-08-24 17:40:41 +0900

    refactor: 지원서 목록 조회 API

--- src/main/java/com/yoyomo/domain/recruitment/domain/repository/ProcessRepository.java
+++ src/main/java/com/yoyomo/domain/recruitment/domain/repository/ProcessRepository.java
@@ -4,7 +4,6 @@ import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ProcessRepository extends JpaRepository<Process, Long> {
