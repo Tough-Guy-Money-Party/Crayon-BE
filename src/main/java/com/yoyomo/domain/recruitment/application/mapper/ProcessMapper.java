b001d498b1b4d28761469176e3b735e5f44f749b
hyxklee <ewgt1234@naver.com>
2024-09-10 21:29:26 +0900

    refactor: 와일드카드로 static import 추가

--- src/main/java/com/yoyomo/domain/recruitment/application/mapper/ProcessMapper.java
+++ src/main/java/com/yoyomo/domain/recruitment/application/mapper/ProcessMapper.java
@@ -1,7 +1,6 @@
 package com.yoyomo.domain.recruitment.application.mapper;
 
 import com.yoyomo.domain.application.application.dto.response.ApplicationResponseDTO;
-import com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO.Save;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
 import org.mapstruct.Mapper;
@@ -11,7 +10,7 @@ import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-import static com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO.Update;
+import static com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO.*;
 import static com.yoyomo.domain.recruitment.application.dto.response.ProcessResponseDTO.Response;
 
 @Mapper(componentModel = "spring",
