108200c26876e54e0dff54b0db9b7771dab27fa8
hyxklee <ewgt1234@naver.com>
2024-09-13 15:19:47 +0900

    feat: 글로벌 예외처리 클래스 추가

--- src/main/java/com/yoyomo/global/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yoyomo/global/config/exception/GlobalExceptionHandler.java
@@ -19,12 +19,17 @@ import java.util.List;
 public class GlobalExceptionHandler {
     private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
 
+    // 사용자 정의 예외처리
     @ExceptionHandler(ApplicationException.class)
     public ResponseDto<Void> handle(ApplicationException ex) {
         log.error(LOG_FORMAT, ex.getClass().getSimpleName(), ex.getErrorCode(), ex.getMessage());
+
+        // 개발 용이성을 위해 전체 에러 로그를 다시 출력. 개발 완료 후 제거할 것
+        log.error("구체적인 에러 로그: ", ex);
         return ResponseDto.of(ex.getErrorCode(), ex.getMessage());
     }
 
+    // @Valid시 발생하는 예외 처리
     @ExceptionHandler(BindException.class)
     public ResponseDto<List<ExceptionDTO>> handle(BindException ex, HttpServletRequest request) {
         int status = 400;
@@ -40,6 +45,32 @@ public class GlobalExceptionHandler {
         }
         log.error(LOG_FORMAT, ex.getClass().getSimpleName(), status, dto);
 
+        // 개발 용이성을 위해 전체 에러 로그를 다시 출력. 개발 완료 후 제거할 것
+        log.error("구체적인 에러 로그: ", ex);
+
         return ResponseDto.of(status, ex.getMessage(), dto);
     }
+
+    // 입력값이 잘못 된 경우 예외처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseDto<Void> handle(IllegalArgumentException ex, HttpServletRequest request) {
+        int status = 400;
+        log.error(LOG_FORMAT, ex.getClass().getSimpleName(), ex.getMessage());
+
+        // 개발 용이성을 위해 전체 에러 로그를 다시 출력. 개발 완료 후 제거할 것
+        log.error("구체적인 에러 로그: ", ex);
+        return ResponseDto.of(status, ex.getMessage());
+    }
+
+    // 위 예외를 제외한 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseDto<Void> handle(Exception ex, HttpServletRequest request) {
+        int status = 500;
+        log.error(LOG_FORMAT, ex.getClass().getSimpleName(), ex.getMessage());
+
+        // 개발 용이성을 위해 전체 에러 로그를 다시 출력. 개발 완료 후 제거할 것
+        log.error("구체적인 에러 로그: ", ex);
+        return ResponseDto.of(status, ex.getMessage());
+    }
+
 }
