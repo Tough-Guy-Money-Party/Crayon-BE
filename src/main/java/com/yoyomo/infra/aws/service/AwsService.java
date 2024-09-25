ec1ce32108d36c203271b9fae569903ef1f6ac23
rootTiket <l20078829@gmail.com>
2024-09-25 20:06:32 +0900

    feat: 유저 서브도메인 배포 삭제 로직 구현

--- src/main/java/com/yoyomo/infra/aws/service/AwsService.java
+++ src/main/java/com/yoyomo/infra/aws/service/AwsService.java
@@ -3,8 +3,8 @@ package com.yoyomo.infra.aws.service;
 
 import com.yoyomo.infra.aws.cloudfront.Service.CloudfrontService;
 import com.yoyomo.infra.aws.route53.service.Route53Service;
+import com.yoyomo.infra.aws.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +12,18 @@ import org.springframework.stereotype.Service;
 public class AwsService {
     private final CloudfrontService cloudfrontService;
     private final Route53Service route53Service;
+    private final S3Service s3Service;
 
     public void distribute(String subDomain) {
 
         String distributeId = cloudfrontService.create(subDomain);
         route53Service.create(subDomain, cloudfrontService.getCloudfrontDomainName(distributeId));
 
+    }
+
+    public void deleteDistribute(String subDomain) {
+        cloudfrontService.disableDitribute(subDomain);
+        s3Service.delete(subDomain);
+        route53Service.delete(subDomain);
     }
 }
