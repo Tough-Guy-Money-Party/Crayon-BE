752e1ffed06b7fd118351c2af21b62df36d7cc0a
hyxklee <ewgt1234@naver.com>
2024-09-25 16:18:27 +0900

    feat: 지원서의 모든 평가를 가져오는 로직 추가

--- src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationRepository.java
@@ -3,9 +3,13 @@ package com.yoyomo.domain.application.domain.repository;
 import com.yoyomo.domain.application.domain.entity.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 
     Optional<Evaluation> findByIdAndDeletedAtIsNull(Long id);
+
+    List<Evaluation> findAllByApplicationIdAndDeletedAtIsNull(UUID applicationId);
 }
