25ec9163f8435e1f7f52e3691750e94a83db4c88
rootTiket <l20078829@gmail.com>
2024-09-27 14:17:34 +0900

    refactor: 스웨거 설명 추가

--- src/main/java/com/yoyomo/infra/aws/presentation/ImageController.java
+++ src/main/java/com/yoyomo/infra/aws/presentation/ImageController.java
@@ -4,6 +4,7 @@ package com.yoyomo.infra.aws.presentation;
 import com.yoyomo.global.common.dto.ResponseDto;
 import com.yoyomo.infra.aws.s3.service.S3Service;
 import com.yoyomo.infra.aws.service.AwsService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +23,7 @@ public class ImageController {
     private final S3Service s3Service;
 
     @PostMapping(value = "/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "[Image] 이미지 업로드")
     public ResponseDto<List<String>> returnImage(@RequestPart("images") List<MultipartFile> images) {
         return ResponseDto.of(HttpStatus.OK.value(),IMAGE_SAVE_SUCCESS.getMessage(), s3Service.save(images));
     }
