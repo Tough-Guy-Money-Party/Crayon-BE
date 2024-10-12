75e0dcb9b94b0ebd3ebda33adc082f212fca89d7
rootTiket <l20078829@gmail.com>
2024-10-12 14:49:12 +0900

    feat: Page청크 가져오는 로직 구현

--- src/main/java/com/yoyomo/YoYoMoBeApplication.java
+++ src/main/java/com/yoyomo/YoYoMoBeApplication.java
@@ -2,9 +2,11 @@ package com.yoyomo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,5 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class YoYoMoBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YoYoMoBeApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 }
