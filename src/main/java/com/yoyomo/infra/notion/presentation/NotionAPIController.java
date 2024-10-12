68674db76105b2f377dbed88932167907e4564b3
rootTiket <l20078829@gmail.com>
2024-10-12 14:50:15 +0900

    refactor: 서머리 추가

--- src/main/java/com/yoyomo/infra/notion/presentation/NotionAPIController.java
+++ src/main/java/com/yoyomo/infra/notion/presentation/NotionAPIController.java
@@ -3,6 +3,7 @@ package com.yoyomo.infra.notion.presentation;
 import com.yoyomo.infra.notion.application.usecase.NotionManageUsecase;
 import com.yoyomo.infra.notion.application.usecase.NotionManageUsecaseImpl;
 import com.yoyomo.infra.notion.service.NotionGetService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class NotionAPIController {
     private final NotionManageUsecaseImpl notionManageUsecaseImpl;
 
     @GetMapping("/page/{pageId}")
+    @Operation(summary = "페이지 가져오기")
     public ResponseEntity<Map<String, Object>> getPage(@PathVariable String pageId) {
         Map<String, Object> pageData = notionManageUsecaseImpl.getPage(pageId);
         return ResponseEntity.ok(pageData);
