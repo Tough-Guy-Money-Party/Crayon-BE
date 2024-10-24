a6b60180c353c31f28278a5b741abc6aa4e6f77b
hyxklee <ewgt1234@naver.com>
2024-10-24 17:46:43 +0900

    feat: 이메일 인증 dto 구현

---
+++ src/main/java/com/yoyomo/domain/application/application/dto/request/ApplicationVerificationRequestDto.java
@@ -0,0 +1,14 @@
+package com.yoyomo.domain.application.application.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class ApplicationVerificationRequestDto {
+
+    public record VerificationRequest (
+            @Email String email,
+            @NotBlank String code
+    ){}
+
+
+}
