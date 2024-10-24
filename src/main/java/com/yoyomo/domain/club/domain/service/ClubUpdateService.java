d9427ee31c6ed1f7d0c4a33b3a88389dcaaecf76
rootTiket <l20078829@gmail.com>
2024-10-24 17:23:50 +0900

    feat: 서브도메인 업데이트 로직 추가

--- src/main/java/com/yoyomo/domain/club/domain/service/ClubUpdateService.java
+++ src/main/java/com/yoyomo/domain/club/domain/service/ClubUpdateService.java
@@ -3,6 +3,7 @@ package com.yoyomo.domain.club.domain.service;
 import com.yoyomo.domain.club.application.dto.request.ClubRequestDTO;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
+import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO.General;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,10 @@ public class ClubUpdateService {
 
     public void update(Club club, String notionPageLink) {
         club.update(notionPageLink);
+    }
+
+    public void update(Club club, General dto){
+        club.update(dto);
     }
 
     public void delete(Club club) {
