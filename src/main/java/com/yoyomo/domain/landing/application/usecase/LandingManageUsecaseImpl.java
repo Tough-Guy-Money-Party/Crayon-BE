f1659ce488dfd4210e2bca739d70850de6f93d1c
rootTiket <l20078829@gmail.com>
2024-10-24 15:45:30 +0900

    feat: 서비스로직 구현

--- src/main/java/com/yoyomo/domain/landing/application/usecase/LandingManageUsecaseImpl.java
+++ src/main/java/com/yoyomo/domain/landing/application/usecase/LandingManageUsecaseImpl.java
@@ -1,9 +1,14 @@
 package com.yoyomo.domain.landing.application.usecase;
 
+import com.yoyomo.domain.club.application.dto.response.ClubResponseDTO.Response;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.club.domain.service.ClubGetService;
 import com.yoyomo.domain.club.domain.service.ClubUpdateService;
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
+import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO;
+import com.yoyomo.domain.landing.application.mapper.LandingMapper;
+import com.yoyomo.domain.landing.domain.entity.Landing;
+import com.yoyomo.domain.landing.domain.service.LandingGetService;
 import com.yoyomo.infra.notion.service.NotionGetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +18,16 @@ import org.springframework.stereotype.Service;
 public class LandingManageUsecaseImpl implements LandingManagementUsecase{
     private final ClubGetService clubGetService;
     private final ClubUpdateService clubUpdateService;
+    private final LandingGetService landingGetService;
     private final NotionGetService notionGetService;
+    private final LandingMapper landingMapper;
+
+    @Override
+    public LandingResponseDTO.General read(String clubId) {
+        Club club = clubGetService.find(clubId);
+        Landing landing = landingGetService.getLanding(club);
+        return landingMapper.toResponse(club,landing);
+    }
 
     @Override
     public void update(LandingRequestDTO.NotionSave dto) {
--- src/main/java/com/yoyomo/domain/landing/application/usecase/LandingManagementUsecase.java
+++ src/main/java/com/yoyomo/domain/landing/application/usecase/LandingManagementUsecase.java
@@ -1,7 +1,10 @@
 package com.yoyomo.domain.landing.application.usecase;
 
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
+import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO;
 
 public interface LandingManagementUsecase {
     void update(LandingRequestDTO.NotionSave dto);
+
+    LandingResponseDTO.General read(String clubId);
 }
