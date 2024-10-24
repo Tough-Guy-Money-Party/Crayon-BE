08ee27782f72795d07d1ff112ea0629b05826aab
rootTiket <l20078829@gmail.com>
2024-10-24 17:25:26 +0900

    feat: 포괄설정 업데이트 usecase 추가

--- src/main/java/com/yoyomo/domain/landing/application/usecase/LandingGeneralManageUsecaseImpl.java
+++ src/main/java/com/yoyomo/domain/landing/application/usecase/LandingGeneralManageUsecaseImpl.java
@@ -3,13 +3,19 @@ package com.yoyomo.domain.landing.application.usecase;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.club.domain.service.ClubGetService;
 import com.yoyomo.domain.club.domain.service.ClubUpdateService;
+import com.yoyomo.domain.club.exception.DuplicatedSubDomainException;
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
+import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO.General;
 import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO;
 import com.yoyomo.domain.landing.application.mapper.LandingMapper;
 import com.yoyomo.domain.landing.domain.entity.Landing;
 import com.yoyomo.domain.landing.domain.service.LandingGetService;
 import com.yoyomo.domain.landing.domain.service.LandingUpdateService;
+import com.yoyomo.infra.aws.s3.service.S3Service;
+import com.yoyomo.infra.aws.service.AwsService;
 import com.yoyomo.infra.notion.service.NotionGetService;
+import jakarta.transaction.Transactional;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,13 +27,43 @@ public class LandingGeneralManageUsecaseImpl implements LandingGeneralManagement
     private final LandingGetService landingGetService;
     private final NotionGetService notionGetService;
     private final LandingMapper landingMapper;
+    private final S3Service s3Service;
+    private final AwsService awsService;
     private final LandingUpdateService landingUpdateService;
+
+    private final String BASEURL = ".crayon.land";
 
     @Override
     public LandingResponseDTO.General readGeneral(String clubId) {
         Club club = clubGetService.find(clubId);
         Landing landing = landingGetService.getLanding(club);
         return landingMapper.toGeneralResponse(club,landing);
+    }
+
+    @Override @Transactional
+    public void update(General dto) throws IOException {
+        Club club = clubGetService.find(dto.clubId());
+        Landing landing = landingGetService.getLanding(club);
+
+        // 서브도메인 변경시 새 배포 시작
+        if (isSubDomainChanged(dto,club)) {
+            String subDomain = checkDuplicatedSubDomain(dto.subDomain()) + BASEURL;
+            s3Service.createBucket(subDomain);
+            awsService.distribute(subDomain);
+            s3Service.save(subDomain);
+        }
+
+        landingUpdateService.update(landing, club, dto);
+    }
+
+    private String checkDuplicatedSubDomain(String subDomain) {
+        if(clubGetService.checkSubDomain(subDomain))
+            throw new DuplicatedSubDomainException();
+        return subDomain;
+    }
+
+    private Boolean isSubDomainChanged(General dto, Club club) {
+        return !dto.subDomain().equals(club.getSubDomain());
     }
 
     @Override
--- src/main/java/com/yoyomo/domain/landing/application/usecase/LandingGeneralManagementUsecase.java
+++ src/main/java/com/yoyomo/domain/landing/application/usecase/LandingGeneralManagementUsecase.java
@@ -2,9 +2,12 @@ package com.yoyomo.domain.landing.application.usecase;
 
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
 import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO;
+import java.io.IOException;
 
 public interface LandingGeneralManagementUsecase {
     void update(LandingRequestDTO.NotionSave dto);
+
+    void update(LandingRequestDTO.General dto) throws IOException;
 
     LandingResponseDTO.General readGeneral(String clubId);
 
