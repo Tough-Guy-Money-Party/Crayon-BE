88a0502dfe27bf39777ad304bb4f4238ea795c96
rootTiket <l20078829@gmail.com>
2024-10-24 16:19:16 +0900

    feat: 성공 메시지를 위한 enum 추가

--- src/main/java/com/yoyomo/domain/landing/presentation/constant/ResponseMessage.java
+++ src/main/java/com/yoyomo/domain/landing/presentation/constant/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
     SUCCESS_READ("랜딩 페이지 조회에 성공했습니다"),
+    SUCCESS_UPDATE("랜딩 페이지 업데이트에서 성공했습니다"),
     LANDING_NOT_FOUND("랜딩 페이지 정보를 찾을 수 없습니다");
 
     private final String message;
