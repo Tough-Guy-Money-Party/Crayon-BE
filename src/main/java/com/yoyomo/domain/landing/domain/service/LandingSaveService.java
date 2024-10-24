a471cc41b1946d860a6a3c708a94a4b0429d916d
rootTiket <l20078829@gmail.com>
2024-10-24 15:40:08 +0900

    fix: 동아리 생성시 랜딩이 생성되도록 변경

---
+++ src/main/java/com/yoyomo/domain/landing/domain/service/LandingSaveService.java
@@ -0,0 +1,17 @@
+package com.yoyomo.domain.landing.domain.service;
+
+import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.landing.domain.entity.Landing;
+import com.yoyomo.domain.landing.domain.repository.LandingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LandingSaveService {
+    private final LandingRepository landingRepository;
+
+    public Landing Save(Club club) {
+        return landingRepository.save(new Landing(club));
+    }
+}
