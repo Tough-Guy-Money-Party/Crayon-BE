a5a1d03426ad56fbdcddd61a29ead8f1a216b25f
rootTiket <l20078829@gmail.com>
2024-10-24 17:25:42 +0900

    feat: 포괄설정 컨트롤러 추가

--- src/main/java/com/yoyomo/domain/landing/presentation/LandingController.java
+++ src/main/java/com/yoyomo/domain/landing/presentation/LandingController.java
@@ -1,5 +1,10 @@
 package com.yoyomo.domain.landing.presentation;
 
+import static com.yoyomo.domain.club.presentation.constant.ResponseMessage.SUCCESS_UPDATE;
+import static com.yoyomo.domain.landing.presentation.constant.ResponseMessage.SUCCESS_READ;
+import static org.springframework.http.HttpStatus.OK;
+
+import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO.NotionSave;
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO.Style;
 import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO;
@@ -8,12 +13,15 @@ import com.yoyomo.domain.landing.application.usecase.LandingGeneralManageUsecase
 import com.yoyomo.domain.landing.application.usecase.LandingStyleManagementUsecaseImpl;
 import com.yoyomo.global.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import static com.yoyomo.domain.club.presentation.constant.ResponseMessage.SUCCESS_UPDATE;
-import static com.yoyomo.domain.landing.presentation.constant.ResponseMessage.SUCCESS_READ;
-import static org.springframework.http.HttpStatus.OK;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "LANDING")
 @RestController()
@@ -32,6 +40,12 @@ public class LandingController {
     @GetMapping("/general/{clubId}")
     public ResponseDto<General> readGeneral(@PathVariable String clubId) {
         return ResponseDto.of(OK.value(), SUCCESS_READ.getMessage(), landingGeneralManageUsecase.readGeneral(clubId));
+    }
+
+    @PatchMapping("/general")
+    public ResponseDto<Void> updateGeneral(LandingRequestDTO.General dto) throws IOException {
+        landingGeneralManageUsecase.update(dto);
+        return ResponseDto.of(OK.value(), SUCCESS_UPDATE.getMessage());
     }
 
     @PatchMapping("/style")
