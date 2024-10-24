ee297af9af61770ae22966dd0bc804e02b3106c2
rootTiket <l20078829@gmail.com>
2024-10-24 15:45:43 +0900

    feat: 컨트롤러 생성

--- src/main/java/com/yoyomo/domain/landing/presentation/LandingController.java
+++ src/main/java/com/yoyomo/domain/landing/presentation/LandingController.java
@@ -1,6 +1,7 @@
 package com.yoyomo.domain.landing.presentation;
 
 import com.yoyomo.domain.landing.application.dto.request.LandingRequestDTO;
+import com.yoyomo.domain.landing.application.dto.response.LandingResponseDTO.General;
 import com.yoyomo.domain.landing.application.usecase.LandingManageUsecaseImpl;
 import com.yoyomo.global.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import static com.yoyomo.domain.club.presentation.constant.ResponseMessage.SUCCESS_UPDATE;
+import static com.yoyomo.domain.landing.presentation.constant.ResponseMessage.SUCCESS_READ;
 import static org.springframework.http.HttpStatus.OK;
 
 @Tag(name = "LANDING")
@@ -20,6 +22,11 @@ public class LandingController {
     @PostMapping()
     public ResponseDto<Void> update(@RequestBody LandingRequestDTO.NotionSave dto) {
         landingManageUsecase.update(dto);
-        return ResponseDto.of(OK.value(),SUCCESS_UPDATE.getMessage());
+        return ResponseDto.of(OK.value(), SUCCESS_UPDATE.getMessage());
+    }
+
+    @GetMapping("/general/{clubId}")
+    public ResponseDto<General> read(@PathVariable String clubId) {
+        return ResponseDto.of(OK.value(),SUCCESS_READ.getMessage(), landingManageUsecase.read(clubId));
     }
 }
