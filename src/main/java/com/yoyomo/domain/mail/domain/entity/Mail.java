454e84960ab0ac63f55bf386a2fcd98a77e3ffd9
hyxklee <ewgt1234@naver.com>
2024-10-31 13:30:02 +0900

    feat: DynamoDB에 저장될 Mail 객체 구현

---
+++ src/main/java/com/yoyomo/domain/mail/domain/entity/Mail.java
@@ -0,0 +1,54 @@
+package com.yoyomo.domain.mail.domain.entity;
+
+import com.yoyomo.domain.mail.application.converter.LocalDateTimeConverter;
+import com.yoyomo.domain.mail.application.dto.MailRequest;
+import com.yoyomo.domain.mail.domain.entity.enums.Status;
+import lombok.*;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbConvertedBy;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@DynamoDbBean
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mail {
+
+    private String id;
+    private String templateId;
+    private Map<String, String> customData;
+    private String to;
+    private String from;
+    private String status;
+    private LocalDateTime scheduledTime;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("id")
+    public String getId(){
+        return id;
+    }
+
+    @DynamoDbConvertedBy(LocalDateTimeConverter.class)
+    public LocalDateTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public static Mail toMail(MailRequest.Reserve dto, String to, Map<String, String> customData){
+        return Mail.builder()
+                .id(UUID.randomUUID().toString())
+                .templateId(dto.templateId())
+                .customData(customData)
+                .to(to)
+                .from(dto.from())
+                .scheduledTime(dto.scheduledTime())
+                .status(Status.SCHEDULED.toString())
+                .build();
+    }
+}
