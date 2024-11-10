124e42e0355a80c1222d4548fa40da695885fe70
hyxklee <ewgt1234@naver.com>
2024-11-10 16:58:32 +0900

    feat[ApplicationManageUseCaseImpl]: 이름으로 지원서 검색 API 페이징 구현

--- src/main/java/com/yoyomo/domain/application/application/usecase/ApplicationManageUseCaseImpl.java
+++ src/main/java/com/yoyomo/domain/application/application/usecase/ApplicationManageUseCaseImpl.java
@@ -20,7 +20,6 @@ import com.yoyomo.domain.user.domain.entity.Manager;
 import com.yoyomo.domain.user.domain.service.UserGetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -59,11 +58,8 @@ public class ApplicationManageUseCaseImpl implements ApplicationManageUseCase {
     public Page<Response> search(String name, String recruitmentId, Long userId, Pageable pageable) {
         Recruitment recruitment = checkAuthorityByRecruitmentId(recruitmentId, userId);
 
-        List<Response> result = applicationGetService.findByName(recruitment, name).stream()
-                .map(applicationMapper::toResponses)
-                .toList();
-
-        return new PageImpl<>(result, pageable, result.size());
+        return applicationGetService.findByName(recruitment, name, pageable)
+                .map(applicationMapper::toResponses);
     }
 
     @Override
@@ -73,9 +69,7 @@ public class ApplicationManageUseCaseImpl implements ApplicationManageUseCase {
 
         return applicationGetService.findAll(process, pageable)
                 .map(application -> applicationMapper.toDetail(
-                        application,
-                        answerGetService.find(application.getAnswerId()),
-                        getEvaluations(application)
+                        application, answerGetService.find(application.getAnswerId()), getEvaluations(application)
                 ));
     }
 
