03023c4e4d8528e7203b563e7ae37efa2fcc7505
hyxklee <ewgt1234@naver.com>
2024-11-10 16:24:28 +0900

    feat[ApplicationRepository]: 지원서를 페이지에 맞게 반환하는 메서드 추가

--- src/main/java/com/yoyomo/domain/application/domain/repository/ApplicationRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/ApplicationRepository.java
@@ -1,7 +1,10 @@
 package com.yoyomo.domain.application.domain.repository;
 
 import com.yoyomo.domain.application.domain.entity.Application;
+import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.user.domain.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,5 +18,7 @@ public interface ApplicationRepository extends JpaRepository<Application, UUID> 
     List<Application> findAllByUser_NameAndDeletedAtIsNull(String name);
 
     Optional<Application> findByIdAndDeletedAtIsNull(UUID id);
+
+    Page<Application> findAllByProcessAndDeletedAtIsNull(Process process, Pageable pageable);
 
 }
