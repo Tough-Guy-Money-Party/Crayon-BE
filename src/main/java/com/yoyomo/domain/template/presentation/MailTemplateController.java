d9b542d83b0315caee11cf7b0ec3291f647c9fb2
hyxklee <ewgt1234@naver.com>
2024-11-13 20:45:37 +0900

    refactor: 컨벤션 관련 수정

--- src/main/java/com/yoyomo/domain/template/presentation/MailTemplateController.java
+++ src/main/java/com/yoyomo/domain/template/presentation/MailTemplateController.java
@@ -26,38 +26,37 @@ public class MailTemplateController {
 
     private final MailTemplateManageUseCase mailTemplateManageUseCase;
 
-
     @GetMapping("/{clubId}")
     @Operation(summary = "이메일 템플릿 목록 조회 API 입니다. 템플릿 ID와 이름만 반환합니다.")
-    public ResponseDto<Page<MailTemplateListResponse>> read(@PathVariable String clubId, @RequestParam Integer page, @RequestParam Integer size){
+    public ResponseDto<Page<MailTemplateListResponse>> read(@PathVariable String clubId, @RequestParam Integer page, @RequestParam Integer size) {
         Page<MailTemplateListResponse> response = mailTemplateManageUseCase.findAll(clubId, PageRequest.of(page, size));
         return ResponseDto.of(HttpStatus.OK.value(), SUCCESS_TEMPLATE_READ.getMessage(), response);
     }
 
     @GetMapping("/detail/{templateId}")
     @Operation(summary = "이메일 템플릿 상세 조회 API 입니다. 템플릿 ID를 이용해 템플릿 전체를 반환합니다.")
-    public ResponseDto<MailTemplateGetResponse> read(@PathVariable String templateId){
+    public ResponseDto<MailTemplateGetResponse> read(@PathVariable String templateId) {
         MailTemplateGetResponse response = mailTemplateManageUseCase.find(templateId);
         return ResponseDto.of(HttpStatus.OK.value(), SUCCESS_TEMPLATE_READ.getMessage(), response);
     }
 
     @PostMapping
     @Operation(summary = "이메일 템플릿 저장 API 입니다.")
-    public ResponseDto<String> save(MailTemplateSaveRequest dto, @CurrentUser @Parameter(hidden = true) Long userId){
+    public ResponseDto<String> save(MailTemplateSaveRequest dto, @CurrentUser @Parameter(hidden = true) Long userId) {
         mailTemplateManageUseCase.save(dto, userId);
         return ResponseDto.of(HttpStatus.OK.value(), SUCCESS_TEMPLATE_SAVE.getMessage());
     }
 
     @PatchMapping("/{templateId}")
     @Operation(summary = "이메일 템플릿 수정 API 입니다.")
-    public ResponseDto<String> update(MailTemplateUpdateRequest dto, @PathVariable String templateId, @CurrentUser @Parameter(hidden = true) Long userId){
+    public ResponseDto<String> update(MailTemplateUpdateRequest dto, @PathVariable String templateId, @CurrentUser @Parameter(hidden = true) Long userId) {
         mailTemplateManageUseCase.update(dto, templateId, userId);
         return ResponseDto.of((HttpStatus.OK.value()), SUCCESS_TEMPLATE_UPDATE.getMessage());
     }
 
     @DeleteMapping("/{templateId}")
     @Operation(summary = "이메일 템플릿 삭제 API 입니다.")
-    public ResponseDto<String> delete(@PathVariable String templateId, @CurrentUser @Parameter(hidden = true) Long userId){
+    public ResponseDto<String> delete(@PathVariable String templateId, @CurrentUser @Parameter(hidden = true) Long userId) {
         mailTemplateManageUseCase.delete(templateId, userId);
         return ResponseDto.of(HttpStatus.OK.value(), SUCCESS_TEMPLATE_DELETE.getMessage());
     }
