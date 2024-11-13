b8db798cef578e8bf3b4ffa8dbd4bed8b4738366
hyxklee <ewgt1234@naver.com>
2024-11-13 13:43:10 +0900

    feat: 객체 업데이트 dto 구현

---
+++ src/main/java/com/yoyomo/domain/template/application/dto/request/MailTemplateUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.yoyomo.domain.template.application.dto.request;
+
+public record MailTemplateUpdateRequest(
+        String customTemplateName,
+        String subject,
+        String htmlPart,
+        String textPart,
+        String clubId
+) {
+}
