9b999930598e2c9f05c338d0a86739f57bf87940
ay-eonii <naayen@naver.com>
2024-11-15 14:28:56 +0900

    style: 개행 및 컨벤션

--- src/main/java/com/yoyomo/domain/recruitment/application/usecase/ProcessManageUseCase.java
+++ src/main/java/com/yoyomo/domain/recruitment/application/usecase/ProcessManageUseCase.java
@@ -1,18 +1,65 @@
 package com.yoyomo.domain.recruitment.application.usecase;
 
-import com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO;
+import com.yoyomo.domain.application.application.dto.response.ApplicationResponseDTO;
+import com.yoyomo.domain.application.application.mapper.ApplicationMapper;
+import com.yoyomo.domain.application.domain.entity.Application;
+import com.yoyomo.domain.application.domain.service.ApplicationGetService;
+import com.yoyomo.domain.recruitment.application.mapper.ProcessMapper;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
+import com.yoyomo.domain.recruitment.domain.service.ProcessDeleteService;
+import com.yoyomo.domain.recruitment.domain.service.ProcessSaveService;
+import com.yoyomo.domain.recruitment.domain.service.RecruitmentGetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
+import static com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO.Save;
+import static com.yoyomo.domain.recruitment.application.dto.request.ProcessRequestDTO.Update;
 import static com.yoyomo.domain.recruitment.application.dto.response.ProcessResponseDTO.Response;
 
-public interface ProcessManageUseCase {
+@Service
+@RequiredArgsConstructor
+public class ProcessManageUseCase {
 
-    List<Process> save(List<ProcessRequestDTO.Save> dto, Recruitment recruitment);
+    private final ProcessSaveService processSaveService;
+    private final ApplicationMapper applicationMapper;
+    private final ProcessMapper processMapper;
+    private final ProcessDeleteService processDeleteService;
+    private final RecruitmentGetService recruitmentGetService;
+    private final ApplicationGetService applicationGetService;
 
-    List<Response> readAll(String recruitmentId);
+    public List<Process> save(List<Save> dto, Recruitment recruitment) {
+        return processSaveService.saveAll(dto, recruitment);
+    }
 
-    List<Process> update(List<ProcessRequestDTO.Update> dto, Recruitment recruitment);
+    /*
+        기존 로직이 ApplicationDTO.Detail로 데이터 변환을 하고 있었는데 필요성을 느끼지 못해 Response로 간소화 하여 구현하였음
+    */
+    public List<Response> readAll(String recruitmentId) {
+        Recruitment recruitment = recruitmentGetService.find(recruitmentId);
+
+        return recruitment.getProcesses().stream()
+                .map(process -> {
+                    List<Application> applications = applicationGetService.findAllInStep(recruitment, process.getStage());
+                    List<ApplicationResponseDTO.Response> responses = applications.stream()
+                            .map(applicationMapper::toResponses)
+                            .toList();
+                    return processMapper.toResponse(process, responses);
+                })
+                .sorted(Comparator.comparingInt(Response::stage))
+                .toList();
+    }
+
+    public List<Process> update(List<Update> dto, Recruitment recruitment) {
+        processDeleteService.deleteAll(recruitment.getProcesses());
+        recruitment.clearProcesses();
+
+        return dto.stream()
+                .map(update -> processSaveService.save(update, recruitment))
+                .sorted(Comparator.comparing(Process::getStage))
+                .toList();
+    }
 }
--- src/main/java/com/yoyomo/domain/recruitment/application/usecase/RecruitmentManageUseCase.java
+++ src/main/java/com/yoyomo/domain/recruitment/application/usecase/RecruitmentManageUseCase.java
@@ -1,23 +1,122 @@
 package com.yoyomo.domain.recruitment.application.usecase;
 
-import com.yoyomo.domain.recruitment.application.dto.request.RecruitmentRequestDTO;
-import com.yoyomo.domain.recruitment.application.dto.response.RecruitmentResponseDTO.DetailResponse;
-import com.yoyomo.domain.recruitment.application.dto.response.RecruitmentResponseDTO.Response;
+import com.yoyomo.domain.club.domain.entity.Club;
+import com.yoyomo.domain.club.domain.service.ClubGetService;
+import com.yoyomo.domain.club.domain.service.ClubManagerAuthService;
+import com.yoyomo.domain.form.application.usecase.FormManageUseCase;
+import com.yoyomo.domain.form.domain.entity.Form;
+import com.yoyomo.domain.form.domain.service.FormGetService;
+import com.yoyomo.domain.form.domain.service.FormUpdateService;
+import com.yoyomo.domain.recruitment.application.dto.response.ProcessResponseDTO;
+import com.yoyomo.domain.recruitment.application.mapper.RecruitmentMapper;
+import com.yoyomo.domain.recruitment.domain.entity.Process;
+import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
+import com.yoyomo.domain.recruitment.domain.service.RecruitmentDeleteService;
+import com.yoyomo.domain.recruitment.domain.service.RecruitmentGetService;
+import com.yoyomo.domain.recruitment.domain.service.RecruitmentSaveService;
+import com.yoyomo.domain.recruitment.domain.service.RecruitmentUpdateService;
+import com.yoyomo.domain.recruitment.exception.RecruitmentDeletedException;
+import com.yoyomo.domain.user.domain.entity.Manager;
+import com.yoyomo.domain.user.domain.service.UserGetService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-public interface RecruitmentManageUseCase {
-    void save(RecruitmentRequestDTO.Save dto, Long userId);
+import java.util.List;
 
-    DetailResponse read(String recruitmentId);
+import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.info;
+import static com.yoyomo.domain.recruitment.application.dto.request.RecruitmentRequestDTO.Save;
+import static com.yoyomo.domain.recruitment.application.dto.request.RecruitmentRequestDTO.Update;
+import static com.yoyomo.domain.recruitment.application.dto.response.RecruitmentResponseDTO.DetailResponse;
+import static com.yoyomo.domain.recruitment.application.dto.response.RecruitmentResponseDTO.Response;
 
-    Page<Response> readAll(Pageable pageable, String clubId);
+@Service
+@RequiredArgsConstructor
+public class RecruitmentManageUseCase {
 
-    void update(String recruitmentId, RecruitmentRequestDTO.Update dto, Long userId);
+    private final UserGetService userGetService;
+    private final ClubGetService clubGetService;
 
-    void close(String recruitmentId, Long userId);
+    private final RecruitmentGetService recruitmentGetService;
+    private final RecruitmentSaveService recruitmentSaveService;
+    private final RecruitmentUpdateService recruitmentUpdateService;
+    private final RecruitmentDeleteService recruitmentDeleteService;
+    private final RecruitmentMapper recruitmentMapper;
 
-    void activate(String recruitmentId, String formId, Long userId);
+    private final FormUpdateService formUpdateService;
+    private final FormGetService formGetService;
+    private final FormManageUseCase formManageUseCase;
+    private final ClubManagerAuthService clubManagerAuthService;
 
-    void cancel(String recruitmentId, Long userId);
+    private final ProcessManageUseCase processManageUseCase;
+
+    @Transactional
+    public void save(Save dto, Long userId) {
+        Club club = clubGetService.find(dto.clubId());
+        Manager manager = userGetService.find(userId);
+        clubManagerAuthService.checkAuthorization(club, manager);
+
+        Recruitment recruitment = recruitmentSaveService.save(dto, club);
+        List<Process> processes = processManageUseCase.save(dto.processes(), recruitment);
+        recruitment.addProcesses(processes);
+    }
+
+    public DetailResponse read(String recruitmentId) {
+        Recruitment recruitment = recruitmentGetService.find(recruitmentId);
+        List<ProcessResponseDTO.Response> processes = processManageUseCase.readAll(recruitmentId);
+        info form = formManageUseCase.readForm(recruitment.getFormId());
+
+        return recruitmentMapper.toDetailResponse(recruitment, processes, form);
+    }
+
+    public Page<Response> readAll(Pageable pageable, String clubId) {
+        Club club = clubGetService.find(clubId);
+        return recruitmentGetService.findAll(club, pageable)
+                .map(recruitmentMapper::toResponse);
+    }
+
+    @Transactional
+    public void update(String recruitmentId, Update dto, Long userId) {
+        Recruitment recruitment = checkAuthorityByRecruitment(recruitmentId, userId);
+        recruitment.checkModifiable();
+        List<Process> processes = processManageUseCase.update(dto.processes(), recruitment);
+        recruitment.addProcesses(processes);
+        recruitmentUpdateService.update(recruitment, dto);
+    }
+
+    public void close(String recruitmentId, Long userId) {
+        Recruitment recruitment = checkAuthorityByRecruitment(recruitmentId, userId);
+        recruitmentUpdateService.delete(recruitment);
+    }
+
+    public void activate(String recruitmentId, String formId, Long userId) {
+        checkDeletedRecruitment(recruitmentId);
+        Recruitment recruitment = checkAuthorityByRecruitment(recruitmentId, userId);
+        Form form = formGetService.find(formId);
+        form.getRecruitmentIds().add(recruitmentId);
+        formUpdateService.update(formId, form.getRecruitmentIds());
+        recruitmentUpdateService.update(recruitment, formId);
+    }
+
+    public void cancel(String recruitmentId, Long userId) {
+        Recruitment recruitment = checkAuthorityByRecruitment(recruitmentId, userId);
+        recruitmentDeleteService.delete(recruitment);
+    }
+
+    private Recruitment checkAuthorityByRecruitment(String recruitmentId, Long userId) {
+        Recruitment recruitment = recruitmentGetService.find(recruitmentId);
+        Manager manager = userGetService.find(userId);
+        recruitment.getClub().checkAuthority(manager);
+
+        return recruitment;
+    }
+
+    private void checkDeletedRecruitment(String recruitmentId) {
+        Recruitment recruitment = recruitmentGetService.find(recruitmentId);
+        if (recruitment.getDeletedAt() != null) {
+            throw new RecruitmentDeletedException();
+        }
+    }
 }
