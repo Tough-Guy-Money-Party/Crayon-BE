cfc62da522033f98d7546963f6599d557160d353
hyxklee <ewgt1234@naver.com>
2024-12-02 16:09:03 +0900

    refactor: 유스케이스 인터페이스 제거

--- src/main/java/com/yoyomo/domain/mail/application/usecase/MailManageUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.yoyomo.domain.mail.application.usecase;
-
-import com.yoyomo.domain.mail.application.dto.request.MailReservationRequest;
-
-public interface MailManageUseCase {
-
-    void reserve(MailReservationRequest dto);
-
-}
