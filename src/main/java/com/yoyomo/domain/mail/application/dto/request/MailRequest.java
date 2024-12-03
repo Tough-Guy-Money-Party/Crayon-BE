3931f0d441c5945bf59f65aabcd75b79320273b2
hyxklee <ewgt1234@naver.com>
2024-12-03 21:01:33 +0900

    refactor: ttl 설정 로직 수정

--- src/main/java/com/yoyomo/domain/mail/application/dto/request/MailRequest.java
+++ src/main/java/com/yoyomo/domain/mail/application/dto/request/MailRequest.java
@@ -4,11 +4,14 @@ import com.yoyomo.domain.mail.domain.entity.Mail;
 import com.yoyomo.domain.mail.domain.entity.enums.Status;
 import com.yoyomo.domain.template.application.dto.request.MailTemplateSaveRequest;
 import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Map;
 import java.util.UUID;
 
+@Slf4j
 public record MailRequest(
         @NotNull Long processId,
         @NotNull LocalDateTime scheduledTime,
@@ -16,6 +19,11 @@ public record MailRequest(
         @NotNull MailTemplateSaveRequest failTemplate
 ){
     public Mail toMail(String source, String destination, Map<String, String> customData, UUID templateId) {
+        // UTC로 변환 후 1시간 추가
+        long ttl = scheduledTime.minusHours(9).plusDays(1)
+                .toInstant(ZoneOffset.UTC)
+                .getEpochSecond();
+
         return Mail.builder()
                 .id(UUID.randomUUID().toString())
                 .templateId(templateId.toString())
@@ -24,6 +32,7 @@ public record MailRequest(
                 .destination(destination)
                 .scheduledTime(scheduledTime())
                 .status(Status.SCHEDULED.toString())
+                .ttl(ttl)
                 .build();
     }
 }
