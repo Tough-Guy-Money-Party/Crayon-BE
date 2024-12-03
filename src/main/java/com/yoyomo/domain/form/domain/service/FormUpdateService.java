94b1247ac38f16c0e4974a8e052bb1f3a55a3590
rootTiket <l20078829@gmail.com>
2024-12-03 22:24:12 +0900

    refactor: service 끼리 참조하지 않도록 변경

--- src/main/java/com/yoyomo/domain/form/domain/service/FormUpdateService.java
+++ src/main/java/com/yoyomo/domain/form/domain/service/FormUpdateService.java
@@ -21,7 +21,6 @@ public class FormUpdateService {
     private final RecruitmentRepository recruitmentRepository;
     private final ItemFactory itemFactory;
     private final FormRepository formRepository;
-    private final FormSaveService formSaveService;
 
     public void update(Form form, String title, String description, List<ItemRequest> rawItems) {
         long linkedRecruitmentCount = recruitmentRepository.countByFormId(form.getId());
@@ -33,7 +32,7 @@ public class FormUpdateService {
                 .map(itemFactory::createItem)
                 .toList();
         form.update(title, description, items);
-        formSaveService.save(form);
+        formRepository.save(form);
     }
 
     public void delete(String formId) {
