bd37f2bd8586fc209d86f16640afb614aa8da320
rootTiket <l20078829@gmail.com>
2024-12-03 22:10:31 +0900

    fix: form 업데이트가 안되는 현상 수정

--- src/main/java/com/yoyomo/domain/form/domain/service/FormUpdateService.java
+++ src/main/java/com/yoyomo/domain/form/domain/service/FormUpdateService.java
@@ -8,11 +8,10 @@ import com.yoyomo.domain.item.application.dto.req.ItemRequest;
 import com.yoyomo.domain.item.domain.entity.Item;
 import com.yoyomo.domain.item.domain.service.factory.ItemFactory;
 import com.yoyomo.domain.recruitment.domain.repository.RecruitmentRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -22,6 +21,7 @@ public class FormUpdateService {
     private final RecruitmentRepository recruitmentRepository;
     private final ItemFactory itemFactory;
     private final FormRepository formRepository;
+    private final FormSaveService formSaveService;
 
     public void update(Form form, String title, String description, List<ItemRequest> rawItems) {
         long linkedRecruitmentCount = recruitmentRepository.countByFormId(form.getId());
@@ -33,6 +33,7 @@ public class FormUpdateService {
                 .map(itemFactory::createItem)
                 .toList();
         form.update(title, description, items);
+        formSaveService.save(form);
     }
 
     public void delete(String formId) {
