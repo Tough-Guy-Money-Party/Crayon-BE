9ecc3308a47f591ca19a0e764c286b81d6b362be
rootTiket <l20078829@gmail.com>
2024-12-03 21:02:20 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/com/yoyomo/domain/form/application/mapper/FormMapper.java
+++ src/main/java/com/yoyomo/domain/form/application/mapper/FormMapper.java
@@ -1,18 +1,17 @@
 package com.yoyomo.domain.form.application.mapper;
 
+import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.DetailResponse;
+import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.Response;
+import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.info;
+
 import com.yoyomo.domain.form.application.dto.request.FormRequestDTO;
 import com.yoyomo.domain.form.domain.entity.Form;
 import com.yoyomo.domain.item.domain.entity.Item;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.List;
-
-import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.DetailResponse;
-import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.Response;
-import static com.yoyomo.domain.form.application.dto.response.FormResponseDTO.info;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
