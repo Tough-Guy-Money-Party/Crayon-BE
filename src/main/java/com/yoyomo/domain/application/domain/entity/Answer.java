1b93bdb01924a6991527e5419f54bc336bc3c0a0
ay-eonii <naayen@naver.com>
2024-12-03 14:11:59 +0900

    fix: Answer applicationId 타입 변경

--- src/main/java/com/yoyomo/domain/application/domain/entity/Answer.java
+++ src/main/java/com/yoyomo/domain/application/domain/entity/Answer.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Builder
@@ -21,7 +20,7 @@ public class Answer {
     @Id
     private String id;
 
-    private UUID applicationId;
+    private String applicationId;
 
     private List<Item> items;
 
