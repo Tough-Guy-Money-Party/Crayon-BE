69821cc15d9d24c2a05717184e08654cf647c982
ay-eonii <naayen@naver.com>
2024-12-04 09:30:37 +0900

    fix: 관리자 지원서 response에서 불필요한 항목 삭제

--- src/main/java/com/yoyomo/domain/application/application/usecase/ApplicationManageUseCase.java
+++ src/main/java/com/yoyomo/domain/application/application/usecase/ApplicationManageUseCase.java
@@ -1,13 +1,10 @@
 package com.yoyomo.domain.application.application.usecase;
 
-import com.yoyomo.domain.application.application.dto.response.EvaluationResponseDTO;
 import com.yoyomo.domain.application.application.mapper.ApplicationMapper;
-import com.yoyomo.domain.application.application.mapper.EvaluationMapper;
 import com.yoyomo.domain.application.domain.entity.Answer;
 import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.domain.service.AnswerGetService;
 import com.yoyomo.domain.application.domain.service.ApplicationGetService;
-import com.yoyomo.domain.application.domain.service.EvaluationGetService;
 import com.yoyomo.domain.club.domain.service.ClubManagerAuthService;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
@@ -16,14 +13,15 @@ import com.yoyomo.domain.recruitment.domain.service.ProcessGetService;
 import com.yoyomo.domain.recruitment.domain.service.RecruitmentGetService;
 import com.yoyomo.domain.user.domain.entity.User;
 import com.yoyomo.domain.user.domain.service.UserGetService;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static com.yoyomo.domain.application.application.dto.request.ApplicationRequestDTO.Stage;
 import static com.yoyomo.domain.application.application.dto.response.ApplicationResponseDTO.Detail;
@@ -39,20 +37,17 @@ public class ApplicationManageUseCase {
     private final AnswerGetService answerGetService;
     private final ProcessGetService processGetService;
     private final RecruitmentGetService recruitmentGetService;
-    private final EvaluationGetService evaluationGetService;
-    private final EvaluationMapper evaluationMapper;
     private final ClubManagerAuthService clubManagerAuthService;
 
     @Transactional(readOnly = true)
     public Detail read(String applicationId, Long userId) {
         Application application = checkAuthorityByApplication(applicationId, userId);
-        List<EvaluationResponseDTO.Response> evaluations = getEvaluations(application);
         Answer answer = answerGetService.findByApplicationId(application.getId());
 
         Recruitment recruitment = recruitmentGetService.find(application.getRecruitmentId());
         List<Type> types = recruitmentGetService.findAllTypesByRecruitment(recruitment);
 
-        return Detail.toDetail(application, answer, evaluations, types);
+        return Detail.toDetail(application, answer, types);
     }
 
     public Page<Response> search(String name, String recruitmentId, Long userId, Pageable pageable) {
@@ -78,7 +73,6 @@ public class ApplicationManageUseCase {
         return applications.map(application -> Detail.toDetail(
                 application,
                 answerMap.get(application.getId()),
-                getEvaluations(application),
                 types
         ));
     }
@@ -108,11 +102,5 @@ public class ApplicationManageUseCase {
         Recruitment recruitment = recruitmentGetService.find(application.getRecruitmentId());
         clubManagerAuthService.checkAuthorization(recruitment.getClub(), manager);
         return application;
-    }
-
-    private List<EvaluationResponseDTO.Response> getEvaluations(Application application) {
-        return evaluationGetService.findAll(application.getId()).stream()
-                .map(evaluationMapper::toResponse)
-                .toList();
     }
 }
