4fdb74f05715e62e62113da784d9a86985ed9e74
hyxklee <ewgt1234@naver.com>
2024-12-09 21:27:27 +0900

    feat: 입력된 지원서들의 프로세스와 상태를 한 번에 업데이트 하는 메서드 구현

--- src/main/java/com/yoyomo/domain/application/domain/repository/ApplicationRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.yoyomo.domain.application.domain.repository;
 
 import com.yoyomo.domain.application.domain.entity.Application;
+import com.yoyomo.domain.application.domain.entity.enums.Status;
 import com.yoyomo.domain.application.domain.repository.dto.ProcessApplicant;
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
@@ -8,7 +9,9 @@ import com.yoyomo.domain.user.domain.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +23,8 @@ public interface ApplicationRepository extends JpaRepository<Application, UUID> 
     List<Application> findAllByUserAndDeletedAtIsNull(User user);
 
     List<Application> findByProcessIdAndDeletedAtIsNull(Long processId, Pageable pageable);
+
+    List<Application> findAllByProcess_IdAndStatusAndDeletedAtIsNull(Long processId, Status status);
 
     Page<Application> findAllByUser_NameAndProcess_RecruitmentAndDeletedAtIsNull(String name, Recruitment recruitment,
                                                                                  Pageable pageable);
@@ -38,4 +43,9 @@ public interface ApplicationRepository extends JpaRepository<Application, UUID> 
             GROUP BY a.process
             """)
     List<ProcessApplicant> countByRecruitmentAndProcess(UUID recruitmentId, List<Process> processes);
+
+    @Modifying
+    @Query("UPDATE Application a SET a.process = :process, a.status = :status WHERE a IN :applications")
+    void updateProcess(@Param("process") Process process, @Param("status") Status status,
+                       @Param("applications") List<Application> applications);
 }
