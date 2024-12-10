97835fd6b6942b4d9f5b9b4ce26929b1b5c02546
ay-eonii <naayen@naver.com>
2024-12-10 21:28:44 +0900

    fix: 유니크 제약조건 지원서Id와 관리자 컬럼에 지정하도록 변경

--- src/main/java/com/yoyomo/domain/application/domain/entity/InterviewRecord.java
+++ src/main/java/com/yoyomo/domain/application/domain/entity/InterviewRecord.java
@@ -10,6 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +25,13 @@ import java.util.UUID;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"manager", "application_id"}
+                )
+        }
+)
 public class InterviewRecord {
 
     @Id
@@ -30,11 +39,11 @@ public class InterviewRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "application_id", nullable = false)
+    @Column(name = "application_id", nullable = false, columnDefinition = "BINARY(16)")
     private UUID applicationId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false, unique = true)
+    @JoinColumn(nullable = false)
     private User manager;
 
     @Column(nullable = false, columnDefinition = "TEXT")
