dd4a62cc2f229ccea852d2945543a9a76f2e3971
hyxklee <ewgt1234@naver.com>
2024-12-12 15:08:19 +0900

    refactor: wrapper 타입으로 변경

--- src/main/java/com/yoyomo/domain/application/application/dto/request/ApplicationMoveRequest.java
+++ src/main/java/com/yoyomo/domain/application/application/dto/request/ApplicationMoveRequest.java
@@ -3,7 +3,7 @@ package com.yoyomo.domain.application.application.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record ApplicationMoveRequest(
-        @NotNull long fromProcessId,
-        @NotNull long toProcessId
+        @NotNull Long fromProcessId,
+        @NotNull Long toProcessId
 ) {
 }
