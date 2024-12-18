aa477cb6baec3e08b9f0730bb59eaec2e5655ad2
rootTiket <l20078829@gmail.com>
2024-12-18 17:14:20 +0900

    feat: email과 userId를 통해서 clubManager를 조회하는 서비스 로직 구현

--- src/main/java/com/yoyomo/domain/club/domain/service/ClubManagerGetService.java
+++ src/main/java/com/yoyomo/domain/club/domain/service/ClubManagerGetService.java
@@ -5,6 +5,8 @@ import com.yoyomo.domain.club.domain.entity.ClubManager;
 import com.yoyomo.domain.club.domain.repository.ClubMangerRepository;
 import com.yoyomo.domain.club.exception.ClubManagerNotFoundException;
 import com.yoyomo.domain.user.domain.entity.User;
+import com.yoyomo.domain.user.domain.repository.UserRepository;
+import com.yoyomo.domain.user.exception.UserNotFoundException;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,23 @@ import org.springframework.stereotype.Service;
 public class ClubManagerGetService {
 
     private final ClubMangerRepository clubMangerRepository;
+    private final UserRepository userRepository;
 
     public ClubManager find(Club club, User manager) {
         return clubMangerRepository.findByClubAndManager(club, manager)
                 .orElseThrow(ClubManagerNotFoundException::new);
+    }
+
+    public ClubManager findByUserId(Club club, long userId) {
+        return clubMangerRepository.findByClubAndUserId(club, userId)
+                .orElseThrow(ClubManagerNotFoundException::new);
+    }
+
+    public ClubManager findByEmail(Club club, String email) {
+        User user = userRepository.findByEmailAndDeletedAtIsNull(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        return find(club, user);
     }
 
     public List<ClubManager> readAllManagers(UUID clubId) {
