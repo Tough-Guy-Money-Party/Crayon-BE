5f4251e16ed2e345370239863b35020be63d0af0
hyxklee <ewgt1234@naver.com>
2024-12-20 13:11:21 +0900

    feat: 예약된 메일이 없을 때 에러 메시지 추가

--- src/main/java/com/yoyomo/domain/mail/presentation/constant/ResponseMessage.java
+++ src/main/java/com/yoyomo/domain/mail/presentation/constant/ResponseMessage.java
@@ -13,6 +13,7 @@ public enum ResponseMessage {
 
     MAIL_DB_UPLOAD_FAIL("메일을 Dynamodb에 업로드하는 중 예외가 발생했습니다."),
     LAMBDA_INVOKE_FAIL("람다 함수를 호출하는 중에 예외가 발생했습니다."),
-    MAIL_CANCEL_FAIL("메일 예약을 취소하는 중 예외가 발생했습니다.");
+    MAIL_CANCEL_FAIL("메일 예약을 취소하는 중 예외가 발생했습니다."),
+    MAIL_NOT_SCHEDULED("예약된 메일이 존재하지 않습니다");
     private final String message;
 }
