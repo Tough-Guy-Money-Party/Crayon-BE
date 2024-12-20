fe1d2fbcfc9f965503044a47fee46b12ced42e59
rootTiket <l20078829@gmail.com>
2024-12-20 17:35:48 +0900

    refactor: 인터페이스 삭제

--- src/main/java/com/yoyomo/domain/club/application/usecase/ClubConfigureUseCase.java
+++ src/main/java/com/yoyomo/domain/club/application/usecase/ClubConfigureUseCase.java
@@ -1,5 +1,9 @@
 package com.yoyomo.domain.club.application.usecase;
 
+import static com.yoyomo.domain.club.application.dto.request.ClubRequestDTO.Save;
+import static com.yoyomo.domain.club.application.dto.request.ClubRequestDTO.Update;
+import static com.yoyomo.domain.club.application.dto.response.ClubResponseDTO.Response;
+
 import com.yoyomo.domain.club.application.mapper.ClubMapper;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.club.domain.service.ClubSaveService;
@@ -7,17 +11,12 @@ import com.yoyomo.domain.club.domain.service.ClubUpdateService;
 import com.yoyomo.domain.club.domain.service.ClubValidateService;
 import com.yoyomo.domain.user.domain.entity.User;
 import com.yoyomo.domain.user.domain.service.UserGetService;
-import com.yoyomo.infra.aws.usecase.DistrubuteUsecaseImpl;
+import com.yoyomo.infra.aws.usecase.DistrubuteUsecase;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.util.UUID;
-
-import static com.yoyomo.domain.club.application.dto.request.ClubRequestDTO.Save;
-import static com.yoyomo.domain.club.application.dto.request.ClubRequestDTO.Update;
-import static com.yoyomo.domain.club.application.dto.response.ClubResponseDTO.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -28,7 +27,7 @@ public class ClubConfigureUseCase {
     private final ClubUpdateService clubUpdateService;
     private final ClubValidateService clubValidateService;
     private final ClubMapper clubMapper;
-    private final DistrubuteUsecaseImpl distributeUsecaseImpl;
+    private final DistrubuteUsecase distributeUsecaseImpl;
 
     @Transactional
     public Response save(Save dto, Long userId) throws IOException {
