e341a5e98ff0688910b61138112214e72fcc7c90
rootTiket <l20078829@gmail.com>
2024-12-22 09:51:53 +0900

    refactor: NotNull 추가

--- src/main/java/com/yoyomo/domain/landing/application/dto/request/CreateSubDomainRequest.java
+++ src/main/java/com/yoyomo/domain/landing/application/dto/request/CreateSubDomainRequest.java
@@ -1,6 +1,9 @@
 package com.yoyomo.domain.landing.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateSubDomainRequest(
+        @NotNull
         String subDomain
 ) {
 }
