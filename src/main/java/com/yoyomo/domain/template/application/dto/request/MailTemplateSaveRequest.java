102bac4c22a7acea5990d9e3e05d25c5f6148a6a
hyxklee <ewgt1234@naver.com>
2024-12-23 22:18:49 +0900

    refactor: dto validation 추가

--- src/main/java/com/yoyomo/domain/template/application/dto/request/MailTemplateSaveRequest.java
+++ src/main/java/com/yoyomo/domain/template/application/dto/request/MailTemplateSaveRequest.java
@@ -2,14 +2,15 @@ package com.yoyomo.domain.template.application.dto.request;
 
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.template.domain.entity.MailTemplate;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 public record MailTemplateSaveRequest(
-        String customTemplateName,
-        String subject,
-        String htmlPart,
-        String textPart
+        @NotBlank String customTemplateName,
+        @NotBlank String subject,
+        @NotBlank String htmlPart,
+        @NotBlank String textPart
 ) {
     public MailTemplate toMailTemplate(Club club) {
         return MailTemplate.builder()
