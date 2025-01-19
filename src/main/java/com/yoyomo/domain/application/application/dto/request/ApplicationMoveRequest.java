3efd6ea5464807ee6c32accf7cc0abd638181b8c
rootTiket <l20078829@gmail.com>
2025-01-19 17:29:03 +0900

    fix: 의도에 맞게 dto 수정

--- src/main/java/com/yoyomo/domain/application/application/dto/request/ApplicationMoveRequest.java
+++ src/main/java/com/yoyomo/domain/application/application/dto/request/ApplicationMoveRequest.java
@@ -3,7 +3,7 @@ package com.yoyomo.domain.application.application.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record ApplicationMoveRequest(
-        @NotNull Long fromProcessId,
-        @NotNull Long toProcessId
+        @NotNull int fromStage,
+        @NotNull int toStage
 ) {
 }
