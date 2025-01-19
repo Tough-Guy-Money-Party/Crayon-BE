f27b89fdfb4817d08149e651ec5074c8c65ba18d
ay-eonii <naayen@naver.com>
2025-01-19 23:11:39 +0900

    fix: 예외 핸들러 통일

--- src/main/java/com/yoyomo/global/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yoyomo/global/config/exception/GlobalExceptionHandler.java
@@ -1,20 +1,15 @@
 package com.yoyomo.global.config.exception;
 
 import com.yoyomo.global.common.dto.ResponseDto;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Slf4j
@@ -93,29 +88,6 @@ public class GlobalExceptionHandler {
     }
 
     // @Valid에서 발생하는 예외 처리
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<ResponseDto<List<ExceptionDTO>>> handle(BindException ex, HttpServletRequest request) {
-        int status = 400;
-        List<ExceptionDTO> dto = new ArrayList<>();
-
-        String url = request.getMethod() + " " + request.getRequestURL().toString();
-
-        if (ex instanceof ErrorResponse) {
-            status = ((ErrorResponse) ex).getStatusCode().value();
-            ex.getBindingResult().getFieldErrors()
-                    .forEach(error -> dto.add(new ExceptionDTO(url, error.getField(), error.getDefaultMessage(), error.getRejectedValue())));
-        } else {
-            dto.add(new ExceptionDTO(url, null, ex.getMessage(), null));
-        }
-
-        log.error(ex.getMessage(), ex);
-        log.error(LOG_FORMAT, ex.getClass().getSimpleName(), status, dto);
-
-        return ResponseEntity
-                .status(status)
-                .body(ResponseDto.of(status, ex.getMessage(), dto));
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto<Void>> handle(MethodArgumentNotValidException ex) {
         int statusCode = ex.getStatusCode().value();
@@ -125,7 +97,7 @@ public class GlobalExceptionHandler {
         log.error(LOG_FORMAT, className, statusCode, error);
 
         return ResponseEntity
-                .status(ex.getStatusCode())
+                .status(statusCode)
                 .body(ResponseDto.of(statusCode, error));
     }
 }
