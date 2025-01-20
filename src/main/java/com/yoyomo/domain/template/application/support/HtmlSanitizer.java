f7c53f48102daa5c50722582709f8c8040f7e267
hyxklee <ewgt1234@naver.com>
2025-01-20 09:38:34 +0900

    refactor: 커스텀 태그 추가

--- src/main/java/com/yoyomo/domain/template/application/support/HtmlSanitizer.java
+++ src/main/java/com/yoyomo/domain/template/application/support/HtmlSanitizer.java
@@ -15,8 +15,8 @@ public class HtmlSanitizer {
         Safelist customSafelist = Safelist.relaxed()
                 // 기본적으로 허용하는 태그들 외에 추가로 허용할 태그들
                 .addTags("span", "ul", "li",
-                        "recruitment-name", "club-name", "applicant",
-                        "interview-date", "interview-place", "process")
+                        "recruitment-name", "club-name", "user-name",
+                        "interview-date", "interview-place", "process", "username")
                 // 모든 태그에 공통으로 허용할 속성
                 .addAttributes(":all", "style", "class", "contenteditable", "data-id")
                 // 특정 태그에 허용할 추가 속성
@@ -27,7 +27,8 @@ public class HtmlSanitizer {
                 .addAttributes("p", "style")
                 .addAttributes("recruitment-name", "data-id", "style")
                 .addAttributes("club-name", "data-id", "style")
-                .addAttributes("applicant", "data-id", "style")
+                .addAttributes("user-name", "data-id", "style")
+                .addAttributes("username", "data-id", "style")
                 .addAttributes("interview-date", "data-id", "style")
                 .addAttributes("interview-place", "data-id", "style")
                 .addAttributes("process", "data-id", "style")
