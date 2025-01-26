b9d33159358ab7f3b5423c7f136a62ccc4e967cf
rootTiket <l20078829@gmail.com>
2025-01-26 16:52:21 +0900

    fix: 내림차순으로 응답하도록 변경

--- src/main/java/com/yoyomo/domain/recruitment/domain/repository/RecruitmentRepository.java
+++ src/main/java/com/yoyomo/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -3,17 +3,16 @@ package com.yoyomo.domain.recruitment.domain.repository;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.form.domain.repository.dto.LinkedRecruitment;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.UUID;
-
 public interface RecruitmentRepository extends JpaRepository<Recruitment, UUID> {
 
-    Page<Recruitment> findAllByClub(Club club, Pageable pageable);
+    Page<Recruitment> findAllByClubOrderByCreatedAtDesc(Club club, Pageable pageable);
 
     long countByFormId(String formId);
 
