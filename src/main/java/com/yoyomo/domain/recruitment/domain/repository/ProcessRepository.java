c419661abb9cb8c76368374d63a77214177300e1
rootTiket <l20078829@gmail.com>
2025-01-26 16:14:36 +0900

    feat: 모집에 해당하는 프로세스를 모두 삭제하는 기능 추가

--- src/main/java/com/yoyomo/domain/recruitment/domain/repository/ProcessRepository.java
+++ src/main/java/com/yoyomo/domain/recruitment/domain/repository/ProcessRepository.java
@@ -2,14 +2,15 @@ package com.yoyomo.domain.recruitment.domain.repository;
 
 import com.yoyomo.domain.recruitment.domain.entity.Process;
 import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProcessRepository extends JpaRepository<Process, Long> {
 
     Optional<Process> findByRecruitmentAndStage(Recruitment recruitment, Integer stage);
 
     List<Process> findAllByRecruitment(Recruitment recruitment);
+
+    void deleteAllByRecruitment(Recruitment recruitment);
 }
--- src/main/java/com/yoyomo/domain/recruitment/domain/service/ProcessDeleteService.java
+++ src/main/java/com/yoyomo/domain/recruitment/domain/service/ProcessDeleteService.java
@@ -1,11 +1,11 @@
 package com.yoyomo.domain.recruitment.domain.service;
 
 import com.yoyomo.domain.recruitment.domain.entity.Process;
+import com.yoyomo.domain.recruitment.domain.entity.Recruitment;
 import com.yoyomo.domain.recruitment.domain.repository.ProcessRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +15,9 @@ public class ProcessDeleteService {
 
     public void deleteAll(List<Process> processes) {
         processRepository.deleteAllInBatch(processes);
+    }
+
+    public void deleteAllByRecruitment(Recruitment recruitment) {
+        processRepository.deleteAllByRecruitment(recruitment);
     }
 }
