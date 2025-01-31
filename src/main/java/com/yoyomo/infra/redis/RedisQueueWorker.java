0f6699f2e6db1bdfc46910c1c1ce8a08954c8085
rootTiket <l20078829@gmail.com>
2025-01-31 17:19:06 +0900

    feat: 레디스 큐에 서브도메인이 enqueue 된 것을 확인하고 파일 업로드 하는 기능 추가

---
+++ src/main/java/com/yoyomo/infra/redis/RedisQueueWorker.java
@@ -0,0 +1,32 @@
+package com.yoyomo.infra.redis;
+
+import com.yoyomo.domain.club.exception.UnavailableSubdomainException;
+import com.yoyomo.infra.aws.s3.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisQueueWorker {
+    private final RedisQueueService redisQueueService;
+    private final S3Service s3Service;
+
+    @Scheduled(fixedDelay = 1000)
+    public void processUploadQueue() {
+        while (true) {
+            String subDomain = redisQueueService.dequeueUpload();
+            if (subDomain == null) {
+                break;
+            }
+
+            try {
+                String fullSubDomain = subDomain + ".crayon.land";
+                s3Service.upload(fullSubDomain);
+            } catch (Exception e) {
+                throw new UnavailableSubdomainException(e.getMessage());
+            }
+        }
+    }
+}
+
