8c92f984aff9e13cbcb7ff1f6f0a2021c6503b1d
나아연 <110026001+ay-eonii@users.noreply.github.com>
2025-02-11 19:52:00 +0900

    CRA-141 평가 메모 수정 및 삭제 (#346)
    * feat: 메모 삭제
    
    * feat: 메모 수정
    
    * fix: 업데이트 쿼리 개선
    
    * fix: 삭제 쿼리 개선
    
    * fix: 사용하지 않는 메서드 삭제

--- src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationMemoRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationMemoRepository.java
@@ -2,11 +2,22 @@ package com.yoyomo.domain.application.domain.repository;
 
 import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.domain.entity.EvaluationMemo;
+import com.yoyomo.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface EvaluationMemoRepository extends JpaRepository<EvaluationMemo, Long> {
-    
+
     List<EvaluationMemo> findAllByProcessIdAndApplication(long processId, Application application);
+
+    @Modifying
+    @Query("DELETE FROM EvaluationMemo em WHERE em.id = :memoId AND em.manager = :manager")
+    int deleteByIdAndManager(long memoId, User manager);
+
+    @Modifying
+    @Query("UPDATE EvaluationMemo em SET em.memo = :memo WHERE em.id = :memoId AND em.manager = :manager")
+    int updateByIdAndManager(String memo, long memoId, User manager);
 }
--- src/main/java/com/yoyomo/domain/application/presentation/EvaluationController.java
+++ src/main/java/com/yoyomo/domain/application/presentation/EvaluationController.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -92,5 +93,24 @@ public class EvaluationController {
         evaluationManageUseCase.createMemo(applicationId, request, userId);
 
         return ResponseDto.of(OK.value(), SUCCESS_SAVE_EVALUATION.getMessage());
+    }
+
+    @DeleteMapping("/memo/{memoId}")
+    @Operation(summary = "평가 메모 삭제")
+    public ResponseDto<Void> deleteMemo(@PathVariable Long memoId,
+                                        @CurrentUser @Parameter(hidden = true) Long userId) {
+        evaluationManageUseCase.deleteMemo(memoId, userId);
+
+        return ResponseDto.of(OK.value(), SUCCESS_DELETE_EVALUATION.getMessage());
+    }
+
+    @PatchMapping("/memo/{memoId}")
+    @Operation(summary = "평가 메모 수정")
+    public ResponseDto<Void> updateMemo(@PathVariable Long memoId,
+                                        @RequestBody @Valid EvaluationMemoRequest request,
+                                        @CurrentUser @Parameter(hidden = true) Long userId) {
+        evaluationManageUseCase.updateMemo(memoId, request, userId);
+
+        return ResponseDto.of(OK.value(), SUCCESS_UPDATE_EVALUATION.getMessage());
     }
 }
