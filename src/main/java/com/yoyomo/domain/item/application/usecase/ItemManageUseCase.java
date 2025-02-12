05eae9df01f769c0cca3d964e3451f15e26032c8
ay-eonii <naayen@naver.com>
2025-02-12 11:24:12 +0900

    fix: 사용하지 않는 메서드 삭제

--- src/main/java/com/yoyomo/domain/item/application/usecase/ItemManageUseCase.java
+++ src/main/java/com/yoyomo/domain/item/application/usecase/ItemManageUseCase.java
@@ -1,14 +1,10 @@
 package com.yoyomo.domain.item.application.usecase;
 
-import com.yoyomo.domain.form.domain.entity.Form;
 import com.yoyomo.domain.item.application.dto.req.ItemRequest;
-import com.yoyomo.domain.item.application.dto.res.ItemResponse;
 import com.yoyomo.domain.item.domain.entity.Item;
 
 import java.util.List;
 
 public interface ItemManageUseCase {
     List<Item> create(List<ItemRequest> request);
-
-    List<ItemResponse> get(Form form);
 }
--- src/main/java/com/yoyomo/domain/item/application/usecase/ItemManageUseCaseImpl.java
+++ src/main/java/com/yoyomo/domain/item/application/usecase/ItemManageUseCaseImpl.java
@@ -1,14 +1,7 @@
 package com.yoyomo.domain.item.application.usecase;
 
-import com.yoyomo.domain.form.domain.entity.Form;
 import com.yoyomo.domain.item.application.dto.req.ItemRequest;
-import com.yoyomo.domain.item.application.dto.res.ItemResponse;
-import com.yoyomo.domain.item.application.mapper.ItemMapper;
-import com.yoyomo.domain.item.domain.entity.Answer;
-import com.yoyomo.domain.item.domain.entity.Date;
 import com.yoyomo.domain.item.domain.entity.Item;
-import com.yoyomo.domain.item.domain.entity.Score;
-import com.yoyomo.domain.item.domain.entity.Select;
 import com.yoyomo.domain.item.domain.service.factory.ItemFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,38 +15,11 @@ import java.util.List;
 public class ItemManageUseCaseImpl implements ItemManageUseCase {
 
     private final ItemFactory itemFactory;
-    private final ItemMapper itemMapper;
 
     @Override
     public List<Item> create(List<ItemRequest> request) {
         return request.stream()
                 .map(itemFactory::createItem)
                 .toList();
-    }
-
-    @Override
-    public List<ItemResponse> get(Form form) {
-        List<Item> items = form.getItems();
-
-        return items.stream()
-                .map(this::getItemResponse)
-                .toList();
-    }
-
-    private ItemResponse getItemResponse(Item item) {
-        if (item instanceof Select) {
-            return itemMapper.mapToSelectResponse((Select) item);
-        }
-        if (item instanceof Score) {
-            return itemMapper.mapToScoreResponse((Score) item);
-        }
-        if (item instanceof Date) {
-            return itemMapper.mapToDateResponse((Date) item);
-        }
-        if (item instanceof Answer) {
-            return itemMapper.mapToAnswerResponse((Answer) item);
-        }
-
-        return itemMapper.mapToItemResponse(item);
     }
 }
