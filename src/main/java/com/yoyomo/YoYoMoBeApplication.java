9f7301beffc179884e8e4e8b4f2b92aee7012dd3
ay-eonii <naayen@naver.com>
2025-02-20 22:26:56 +0900

    refactor: Mongo 설정 분할

--- src/main/java/com/yoyomo/YoYoMoBeApplication.java
+++ src/main/java/com/yoyomo/YoYoMoBeApplication.java
@@ -3,12 +3,10 @@ package com.yoyomo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableMongoAuditing
 @EnableScheduling
 public class YoYoMoBeApplication {
     public static void main(String[] args) {
