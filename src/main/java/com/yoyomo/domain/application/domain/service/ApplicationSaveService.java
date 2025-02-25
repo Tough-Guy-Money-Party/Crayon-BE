c9f4a27fbb950303d73765180be75a81bfb495b8
ay-eonii <naayen@naver.com>
2025-02-25 10:58:20 +0900

    feat: DB단에서 동시성 제어하도록 변경

--- src/main/java/com/yoyomo/domain/application/domain/service/ApplicationSaveService.java
+++ src/main/java/com/yoyomo/domain/application/domain/service/ApplicationSaveService.java
@@ -4,13 +4,11 @@ import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.domain.repository.ApplicationRepository;
 import com.yoyomo.domain.recruitment.domain.repository.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ApplicationSaveService {
