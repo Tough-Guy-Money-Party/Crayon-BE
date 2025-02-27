80e7466fa9522f87995a446ad35cd94a355ddaf8
rootTiket <l20078829@gmail.com>
2025-02-27 15:37:57 +0900

    refactor: 큐 관리 로직 분리

--- src/main/java/com/yoyomo/infra/redis/RedisPublisher.java
+++ src/main/java/com/yoyomo/infra/redis/RedisPublisher.java
@@ -2,22 +2,18 @@ package com.yoyomo.infra.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class RedisPublisher {
-    private static final String QUEUE_KEY = "upload:queue";
     private static final String QUEUE_TOPIC = "upload:topic";
-    private final RedisTemplate<String, String> queueRedisTemplate;
+
     private final RedisTemplate<String, String> redisTemplate;
+    private final RedisQueueService redisQueueService;
 
     public void publishCreation(String subDomain) {
-        queueRedisTemplate.opsForList().rightPush(QUEUE_KEY, subDomain);
+        redisQueueService.enqueue(subDomain);
         redisTemplate.convertAndSend(QUEUE_TOPIC, subDomain);
-    }
-
-    public String dequeueUpload() {
-        return queueRedisTemplate.opsForList().leftPop(QUEUE_KEY);
     }
 }
