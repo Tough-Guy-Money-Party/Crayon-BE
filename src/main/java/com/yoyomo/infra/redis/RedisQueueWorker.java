a799285b26873129b0c44d030348f3e8f238c5f1
rootTiket <l20078829@gmail.com>
2025-02-27 15:34:46 +0900

    fix: 한 요청에서 큐가 빌 때 까지 실행하지 않도록 변경

--- src/main/java/com/yoyomo/infra/redis/RedisQueueWorker.java
+++ src/main/java/com/yoyomo/infra/redis/RedisQueueWorker.java
@@ -1,11 +1,11 @@
 package com.yoyomo.infra.redis;
 
+import com.mongodb.lang.Nullable;
 import com.yoyomo.domain.club.exception.UnavailableSubdomainException;
 import com.yoyomo.infra.aws.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,24 +13,23 @@ import org.springframework.stereotype.Component;
 public class RedisQueueWorker implements MessageListener {
     private static final String DOMAIN_FORMAT = "%s.crayon.land";
 
-    private final RedisPublisher redisPublisher;
     private final S3Service s3Service;
+    private final RedisQueueService redisQueueService;
 
     @Override
     public void onMessage(Message message, @Nullable byte[] pattern) {
-        processUploadQueue();
+        processUpload();
     }
 
-    public void processUploadQueue() {
-        String subDomain = redisPublisher.dequeueUpload();
-        while (subDomain != null) {
-            try {
-                String fullSubDomain = String.format(DOMAIN_FORMAT, subDomain);
-                s3Service.upload(fullSubDomain);
-            } catch (Exception e) {
-                throw new UnavailableSubdomainException(e.getMessage());
-            }
-            subDomain = redisPublisher.dequeueUpload();
+
+    public void processUpload() {
+        String subDomain = redisQueueService.dequeue();
+        try {
+            String fullSubDomain = String.format(DOMAIN_FORMAT, subDomain);
+            s3Service.upload(fullSubDomain);
+        } catch (Exception e) {
+            redisQueueService.enqueueToRetryQueue(subDomain);
+            throw new UnavailableSubdomainException(e.getMessage());
         }
     }
 }
