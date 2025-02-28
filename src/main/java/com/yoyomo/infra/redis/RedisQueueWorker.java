41528cbb2f0d47f2dc79065f6bd12ea3f1e1b811
rootTiket <l20078829@gmail.com>
2025-02-28 15:55:21 +0900

    fix: 재시도 정책 변경

--- src/main/java/com/yoyomo/infra/redis/RedisQueueWorker.java
+++ src/main/java/com/yoyomo/infra/redis/RedisQueueWorker.java
@@ -1,12 +1,10 @@
 package com.yoyomo.infra.redis;
 
-import com.mongodb.lang.Nullable;
 import com.yoyomo.domain.club.exception.UnavailableSubdomainException;
 import com.yoyomo.infra.aws.s3.service.S3Service;
 import com.yoyomo.infra.redis.service.RedisQueueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
@@ -14,25 +12,29 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @EnableAsync
-public class RedisQueueWorker implements MessageListener {
+public class RedisQueueWorker {
     private static final String DOMAIN_FORMAT = "%s.crayon.land";
 
     private final S3Service s3Service;
     private final RedisQueueService redisQueueService;
 
-    @Override
-    public void onMessage(Message message, @Nullable byte[] pattern) {
-        processUpload();
-    }
-
     @Async
-    public void processUpload() {
-        String subDomain = redisQueueService.dequeue();
+    @EventListener
+    public void processUpload(String subDomain) {
+        String fullSubDomain = String.format(DOMAIN_FORMAT, subDomain);
         try {
-            String fullSubDomain = String.format(DOMAIN_FORMAT, subDomain);
             s3Service.upload(fullSubDomain);
         } catch (Exception e) {
-            redisQueueService.enqueueToRetryQueue(subDomain);
+            retryUpload(fullSubDomain);
+            throw new UnavailableSubdomainException(e.getMessage());
+        }
+    }
+
+    private void retryUpload(String fullSubDomain) {
+        try {
+            s3Service.upload(fullSubDomain);
+        } catch (Exception e) {
+            redisQueueService.enqueueToFailedQueue(fullSubDomain);
             throw new UnavailableSubdomainException(e.getMessage());
         }
     }
