0c362509a00bad4b4b9c5e6a7c42b5f32891d54d
rootTiket <l20078829@gmail.com>
2025-02-28 15:54:51 +0900

    fix: EventPublisher를 사용하도록 변경

--- src/main/java/com/yoyomo/infra/redis/RedisPublisher.java
+++ src/main/java/com/yoyomo/infra/redis/RedisPublisher.java
@@ -2,19 +2,18 @@ package com.yoyomo.infra.redis;
 
 import com.yoyomo.infra.redis.service.RedisQueueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class RedisPublisher {
-    private static final String QUEUE_TOPIC = "upload:topic";
 
-    private final RedisTemplate<String, String> queueRedisTemplate;
     private final RedisQueueService redisQueueService;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public void publishCreation(String subDomain) {
         redisQueueService.enqueue(subDomain);
-        queueRedisTemplate.convertAndSend(QUEUE_TOPIC, subDomain);
+        applicationEventPublisher.publishEvent(subDomain);
     }
 }
