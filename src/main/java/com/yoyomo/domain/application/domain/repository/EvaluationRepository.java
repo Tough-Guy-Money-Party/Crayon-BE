d7fbef2ed28b97438eeba79143d5169ad04991a1
ay-eonii <naayen@naver.com>
2025-03-02 14:51:50 +0900

    refactor: Evaluation 조회 시 User fetch join

--- src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationRepository.java
@@ -3,6 +3,7 @@ package com.yoyomo.domain.application.domain.repository;
 import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.domain.entity.Evaluation;
 import com.yoyomo.domain.user.domain.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 
     List<Evaluation> findAllByApplicationId(UUID applicationId);
 
+    @EntityGraph(attributePaths = "manager")
     List<Evaluation> findAllByApplication(Application application);
 
     boolean existsByManagerAndApplication(User manager, Application application);
