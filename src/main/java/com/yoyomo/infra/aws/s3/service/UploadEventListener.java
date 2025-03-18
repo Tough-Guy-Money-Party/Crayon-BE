f432ad1503eab875a102b2ec4165bb20b5151ac9
rootTiket <l20078829@gmail.com>
2025-03-19 00:20:16 +0900

    refactor: 배포 책임을 이벤트리스너로 이전

--- src/main/java/com/yoyomo/infra/aws/s3/service/UploadEventListener.java
+++ src/main/java/com/yoyomo/infra/aws/s3/service/UploadEventListener.java
@@ -1,7 +1,9 @@
 package com.yoyomo.infra.aws.s3.service;
 
 import com.yoyomo.domain.club.exception.UnavailableSubdomainException;
+import com.yoyomo.infra.aws.cloudfront.Service.CloudfrontService;
 import com.yoyomo.infra.aws.dto.LandingClientUploadEvent;
+import com.yoyomo.infra.aws.route53.service.Route53Service;
 import com.yoyomo.infra.redis.service.RedisQueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -14,6 +16,9 @@ import org.springframework.stereotype.Component;
 @EnableAsync
 public class UploadEventListener {
     private final S3Service s3Service;
+    private final CloudfrontService cloudfrontService;
+    private final Route53Service route53Service;
+
     private final RedisQueueService redisQueueService;
 
     @Async
@@ -21,6 +26,11 @@ public class UploadEventListener {
     public void processUpload(LandingClientUploadEvent uploadEvent) {
         String subdomain = uploadEvent.subdomain();
         try {
+            s3Service.createBucket(subdomain);
+            String distributeId = cloudfrontService.create(subdomain);
+            String cloudfrontDomainName = cloudfrontService.getCloudfrontDomainName(distributeId);
+            route53Service.create(subdomain, cloudfrontDomainName);
+
             s3Service.upload(subdomain);
         } catch (Exception e) {
             retryUpload(subdomain);
