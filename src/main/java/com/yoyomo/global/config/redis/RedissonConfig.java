8795f3ac1c843f2856da84ae970e85583b249886
rootTiket <l20078829@gmail.com>
2025-05-20 12:27:40 +0900

    HOTFIX: redis password 설정

--- src/main/java/com/yoyomo/global/config/redis/RedissonConfig.java
+++ src/main/java/com/yoyomo/global/config/redis/RedissonConfig.java
@@ -10,19 +10,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    private static final String REDISSON_HOST_FORMAT = "redis://%s:%d";
+	private static final String REDISSON_HOST_FORMAT = "redis://%s:%d";
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+	@Value("${spring.data.redis.host}")
+	private String host;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
+	@Value("${spring.data.redis.port}")
+	private int port;
 
-    @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer().setAddress(String.format(REDISSON_HOST_FORMAT, host, port));
+	@Value("${spring.data.redis.password}")
+	private String password;
 
-        return Redisson.create(config);
-    }
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		config.useSingleServer().setAddress(String.format(REDISSON_HOST_FORMAT, host, port))
+			.setPassword(password);
+
+		return Redisson.create(config);
+	}
 }
