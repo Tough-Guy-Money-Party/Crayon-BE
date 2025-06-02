79fcca6c21adbfafaf7016c08a00924a70a2faeb
ay-eonii <naayen@naver.com>
2025-06-02 14:15:51 +0900

    test: 문항, 질문 개수 불일치 예외

--- src/test/java/com/yoyomo/domain/application/application/usecase/ApplicationImportUseCaseTest.java
+++ src/test/java/com/yoyomo/domain/application/application/usecase/ApplicationImportUseCaseTest.java
@@ -9,6 +9,7 @@ import com.yoyomo.domain.application.domain.entity.Answer;
 import com.yoyomo.domain.application.domain.entity.Application;
 import com.yoyomo.domain.application.domain.repository.ApplicationRepository;
 import com.yoyomo.domain.application.domain.repository.mongo.AnswerRepository;
+import com.yoyomo.domain.application.exception.QuestionReplySizeMismatchException;
 import com.yoyomo.domain.club.domain.entity.Club;
 import com.yoyomo.domain.club.domain.repository.ClubMangerRepository;
 import com.yoyomo.domain.club.domain.repository.ClubRepository;
@@ -25,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ApplicationImportUseCaseTest extends ApplicationTest {
@@ -120,5 +122,52 @@ class ApplicationImportUseCaseTest extends ApplicationTest {
                 () -> assertThat(answers.get(0).getApplicationId()).isNotNull(),
                 () -> assertThat(answers.get(0).getItems()).hasSize(3)
         );
+    }
+
+    @Test
+    void importApplications_MismatchSizeException() {
+        // given
+        User user = userRepository.save(TestFixture.user());
+        Club club = clubRepository.save(TestFixture.club());
+        clubMangerRepository.save(TestFixture.clubManager(club, user));
+
+        Process process = processRepository.save(TestFixture.process(1));
+        Recruitment recruitment = recruitmentRepository.save(TestFixture.recruitment(club, process));
+        process.addRecruitment(recruitment);
+        processRepository.save(process);
+
+        List<QuestionRequest> questionRequests = List.of(
+                new QuestionRequest("이름을 적어", "string"),
+                new QuestionRequest("입력해라 전화번호", "string"),
+                new QuestionRequest("언제볼래", "datetime"),
+                new QuestionRequest("배고프세요?", "string"),
+                new QuestionRequest("졸리세요?", "string")
+        );
+
+        List<RespondentRequest> respondentRequests = List.of(
+                new RespondentRequest(
+                        List.of(
+                                new DataRequest("나아연"),
+                                new DataRequest("01012345678"),
+                                new DataRequest("Date(2025,05,30,19,36,00)"),
+                                new DataRequest("죽겠어요"),
+                                new DataRequest(null)
+                        )
+                ),
+                new RespondentRequest(
+                        List.of(
+                                new DataRequest("이근표"),
+                                new DataRequest("01087654321"),
+                                new DataRequest("Date(2025,05,30,19,36,00)"),
+                                new DataRequest("기절")
+                        )
+                )
+        );
+
+        ApplicationImportRequest applicationImportRequest = new ApplicationImportRequest(questionRequests, respondentRequests);
+
+        // when & then
+        assertThatThrownBy(() -> applicationImportUseCase.importApplications(recruitment.getId(), user, applicationImportRequest))
+                .isInstanceOf(QuestionReplySizeMismatchException.class);
     }
 }
