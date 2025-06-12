c64cc69f7f8e4e22555530b3d783225fab7f5ba2
rootTiket <l20078829@gmail.com>
2025-06-12 19:23:46 +0900

    feat: 모집 id 하위의 모든 평가를 삭제하는 로직 구현

--- src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationMemoRepository.java
+++ src/main/java/com/yoyomo/domain/application/domain/repository/EvaluationMemoRepository.java
@@ -1,23 +1,31 @@
 package com.yoyomo.domain.application.domain.repository;
 
-import com.yoyomo.domain.application.domain.entity.Application;
-import com.yoyomo.domain.application.domain.entity.EvaluationMemo;
-import com.yoyomo.domain.user.domain.entity.User;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import com.yoyomo.domain.application.domain.entity.Application;
+import com.yoyomo.domain.application.domain.entity.EvaluationMemo;
+import com.yoyomo.domain.user.domain.entity.User;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface EvaluationMemoRepository extends JpaRepository<EvaluationMemo, Long> {
 
-    List<EvaluationMemo> findAllByApplication(Application application);
+	List<EvaluationMemo> findAllByApplication(Application application);
 
-    @Modifying
-    @Query("DELETE FROM EvaluationMemo em WHERE em.id = :memoId AND em.manager = :manager")
-    int deleteByIdAndManager(long memoId, User manager);
+	@Modifying
+	@Query("DELETE FROM EvaluationMemo em WHERE em.id = :memoId AND em.manager = :manager")
+	int deleteByIdAndManager(long memoId, User manager);
 
-    @Modifying
-    @Query("UPDATE EvaluationMemo em SET em.memo = :memo WHERE em.id = :memoId AND em.manager = :manager")
-    int updateByIdAndManager(String memo, long memoId, User manager);
+	@Modifying
+	@Query("UPDATE EvaluationMemo em SET em.memo = :memo WHERE em.id = :memoId AND em.manager = :manager")
+	int updateByIdAndManager(String memo, long memoId, User manager);
+
+	@Modifying
+	@Query("DELETE FROM EvaluationMemo em WHERE em.application.recruitmentId = :recruitmentId")
+	void deleteByRecruitmentId(@Param("recruitmentId") UUID recruitmentId);
 }
