0cafdc29aa06325eebf846b2da38655b3c951410
나아연 <110026001+ay-eonii@users.noreply.github.com>
2025-07-08 17:44:38 +0900

    CRA-173 구글폼 연동 기능 지원일 매핑 (#418)
    * feat: 지원자 정보에 지원일 추가
    
    * feat: 지원일 `createdAt`에 매핑
    
    * test: 지원일 매핑 테스트
    
    * refactor: optional 처리 로직 수정

--- src/main/java/com/yoyomo/domain/application/domain/vo/Applicant.java
+++ src/main/java/com/yoyomo/domain/application/domain/vo/Applicant.java
@@ -1,33 +1,45 @@
 package com.yoyomo.domain.application.domain.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import com.yoyomo.global.common.util.DateFormatter;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
 @AllArgsConstructor
 public class Applicant {
 
-    private final Map<ApplicantInfo, Reply> values;
+	private final Map<ApplicantInfo, Reply> values;
 
-    public static Applicant from(List<QuestionReply> questionReplies) {
-        Map<ApplicantInfo, Reply> applicantInfos = questionReplies.stream()
-                .collect(Collectors.toMap(ApplicantInfo::find, QuestionReply::getReply));
-        return new Applicant(applicantInfos);
-    }
+	public static Applicant from(List<QuestionReply> questionReplies) {
+		Map<ApplicantInfo, Reply> applicantInfos = questionReplies.stream()
+			.collect(Collectors.toMap(ApplicantInfo::find, QuestionReply::getReply));
+		return new Applicant(applicantInfos);
+	}
 
-    public String getName() {
-        return values.getOrDefault(ApplicantInfo.NAME, Reply.empty()).value();
-    }
+	public String getName() {
+		return values.getOrDefault(ApplicantInfo.NAME, Reply.empty()).value();
+	}
 
-    public String getPhone() {
-        return values.getOrDefault(ApplicantInfo.PHONE, Reply.empty()).value();
-    }
+	public String getPhone() {
+		return values.getOrDefault(ApplicantInfo.PHONE, Reply.empty()).value();
+	}
 
-    public String getEmail() {
-        return values.getOrDefault(ApplicantInfo.EMAIL, Reply.empty()).value();
-    }
+	public String getEmail() {
+		return values.getOrDefault(ApplicantInfo.EMAIL, Reply.empty()).value();
+	}
+
+	public Optional<LocalDateTime> getAppliedAt() {
+		Reply reply = values.getOrDefault(ApplicantInfo.APPLIED_AT, Reply.empty());
+		if (reply.isEmpty()) {
+			return Optional.empty();
+		}
+		return Optional.of(DateFormatter.parse(reply.value()));
+	}
 }
