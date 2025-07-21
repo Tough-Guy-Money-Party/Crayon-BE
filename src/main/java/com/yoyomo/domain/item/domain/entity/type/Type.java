6f9a84a488f8ba8a1d039418a7887791cb6d6c84
ay-eonii <naayen@naver.com>
2025-07-21 13:26:29 +0900

    test: 지원 테스트

--- src/main/java/com/yoyomo/domain/item/domain/entity/type/Type.java
+++ src/main/java/com/yoyomo/domain/item/domain/entity/type/Type.java
@@ -1,12 +1,11 @@
 package com.yoyomo.domain.item.domain.entity.type;
 
 public enum Type {
-    SHORT_FORM,
-    SHORT,
-    LONG_FORM,
-    SELECT,
-    MULTI_SELECT,
-    CALENDAR,
-    SCORE,
-    ANNOUNCE
+	SHORT_FORM,
+	LONG_FORM,
+	SELECT,
+	MULTI_SELECT,
+	CALENDAR,
+	SCORE,
+	ANNOUNCE
 }
--- src/test/java/com/yoyomo/domain/ApplicationTest.java
+++ src/test/java/com/yoyomo/domain/ApplicationTest.java
@@ -1,9 +1,5 @@
 package com.yoyomo.domain;
 
-import com.yoyomo.domain.application.application.usecase.ApplicationVerifyUseCase;
-import com.yoyomo.domain.fixture.CustomRepository;
-import com.yoyomo.global.config.jwt.JwtProvider;
-import com.yoyomo.global.config.kakao.KakaoServiceNew;
 import org.junit.jupiter.api.AfterEach;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,31 +9,60 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.yoyomo.domain.application.application.usecase.ApplicationVerifyUseCase;
+import com.yoyomo.domain.application.domain.repository.ApplicationRepository;
+import com.yoyomo.domain.club.domain.repository.ClubMangerRepository;
+import com.yoyomo.domain.club.domain.repository.ClubRepository;
+import com.yoyomo.domain.fixture.CustomRepository;
+import com.yoyomo.domain.recruitment.domain.repository.ProcessRepository;
+import com.yoyomo.domain.recruitment.domain.repository.RecruitmentRepository;
+import com.yoyomo.domain.user.domain.repository.UserRepository;
+import com.yoyomo.global.config.jwt.JwtProvider;
+import com.yoyomo.global.config.kakao.KakaoServiceNew;
+
 @MockBean(classes = {
-        ApplicationVerifyUseCase.class,
-        KakaoServiceNew.class,
-        JwtProvider.class,
-        RedissonClient.class
+	ApplicationVerifyUseCase.class,
+	KakaoServiceNew.class,
+	JwtProvider.class,
+	RedissonClient.class
 })
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.REGEX,
-                pattern = {
-                        "com.yoyomo.domain.mail.*",
-                        "com.yoyomo.domain.landing.*",
-                        "com.yoyomo.domain.template.*"
-                }
-        )
+	excludeFilters = @ComponentScan.Filter(
+		type = FilterType.REGEX,
+		pattern = {
+			"com.yoyomo.domain.mail.*",
+			"com.yoyomo.domain.landing.*",
+			"com.yoyomo.domain.template.*"
+		}
+	)
 )
 @SpringBootTest
 @ActiveProfiles("test")
 public class ApplicationTest {
 
-    @Autowired
-    CustomRepository customRepository;
+	@Autowired
+	CustomRepository customRepository;
 
-    @AfterEach
-    void tearDown() {
-        customRepository.clearAndReset();
-    }
+	@Autowired
+	protected UserRepository userRepository;
+
+	@Autowired
+	protected ClubRepository clubRepository;
+
+	@Autowired
+	protected ClubMangerRepository clubMangerRepository;
+
+	@Autowired
+	protected RecruitmentRepository recruitmentRepository;
+
+	@Autowired
+	protected ProcessRepository processRepository;
+
+	@Autowired
+	protected ApplicationRepository applicationRepository;
+
+	@AfterEach
+	void tearDown() {
+		customRepository.clearAndReset();
+	}
 }
